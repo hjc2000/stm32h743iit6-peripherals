a88bb0d655b75df5ce042d033caa2a88ff3050dd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-10 22:43:40 +0800

    base::LockGuard l{_read_lock};

--- private_src/Serial.cpp
+++ private_src/Serial.cpp
@@ -149,7 +149,7 @@ int32_t bsp::Serial::Read(uint8_t *buffer, int32_t offset, int32_t count)
         throw std::invalid_argument{"count 太大"};
     }
 
-    task::MutexLockGuard l{_read_lock};
+    base::LockGuard l{_read_lock};
     while (true)
     {
         DI_InterruptSwitch().DoGlobalCriticalWork(
