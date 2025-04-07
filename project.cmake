44b3d982b3cafc732b914ba17f0b0757ef75b428
hjc2000 <hjc.666@foxmail.com>
2025-04-07 09:24:27 +0800

    OBJECT

--- project.cmake
+++ project.cmake
@@ -1,5 +1,6 @@
-add_library(${ProjectName})
+add_library(${ProjectName} OBJECT)
 target_import_src(${ProjectName})
+target_install_obj_dir(${ProjectName})
 
 target_import_base(${ProjectName} PUBLIC)
 target_import_bsp_interface(${ProjectName} PUBLIC)
