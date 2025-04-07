b73fa7b47dda7d328d96c0f714ec41fad95c7f7f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-07 19:27:14 +0800

    _hse_clk_hjc_init

--- private_src/clock/clock-signal/SysclkClockSignal.h
+++ private_src/clock/clock-signal/SysclkClockSignal.h
@@ -2,30 +2,29 @@
 #include <base/define.h>
 #include <bsp-interface/clock/IClockSignal.h>
 #include <bsp-interface/di/interrupt.h>
-#include <bsp-interface/TaskSingletonGetter.h>
 #include <hal.h>
 
 namespace bsp
 {
-    class SysclkClockSignal :
-        public bsp::IClockSignal
-    {
-    public:
-        static_function SysclkClockSignal &Instance();
+	class SysclkClockSignal :
+		public bsp::IClockSignal
+	{
+	public:
+		static_function SysclkClockSignal &Instance();
 
-        /// @brief 时钟信号的名称。
-        /// @return
-        virtual std::string Name() const override;
+		/// @brief 时钟信号的名称。
+		/// @return
+		virtual std::string Name() const override;
 
-        /// @brief 时钟信号的频率
-        /// @return
-        virtual base::MHz Frequency() const override;
+		/// @brief 时钟信号的频率
+		/// @return
+		virtual base::MHz Frequency() const override;
 
-        /// @brief 打开时钟信号。
-        /// @param output_division_factor 输出分频系数。
-        /// @param clock_source 时钟源。像 stm32 的系统时钟 sysclk，是时钟源后的第一个时钟信号，输入端连接着
-        /// 各个时钟源，输出端供给各个子时钟信号。本参数用来选择类似 sysclk 这种时钟信号的时钟源。
-        virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor,
-                          IClockSignal_ClockSource const &clock_source) override;
-    };
+		/// @brief 打开时钟信号。
+		/// @param output_division_factor 输出分频系数。
+		/// @param clock_source 时钟源。像 stm32 的系统时钟 sysclk，是时钟源后的第一个时钟信号，输入端连接着
+		/// 各个时钟源，输出端供给各个子时钟信号。本参数用来选择类似 sysclk 这种时钟信号的时钟源。
+		virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor,
+						  IClockSignal_ClockSource const &clock_source) override;
+	};
 } // namespace bsp
--- private_src/clock/di/DI_ClockSignalCollection.cpp
+++ private_src/clock/di/DI_ClockSignalCollection.cpp
@@ -1,5 +1,6 @@
 #include <base/container/Dictionary.h>
 #include <bsp-interface/di/clock.h>
+#include <bsp-interface/TaskSingletonGetter.h>
 #include <D1Pclk1ClockSignal.h>
 #include <D3Pclk1ClockSignal.h>
 #include <HclkClockSignal.h>
@@ -9,36 +10,36 @@
 
 namespace
 {
-    class Initializer
-    {
-    public:
-        Initializer()
-        {
-            _dic.Add(bsp::SysclkClockSignal::Instance().Name(), &bsp::SysclkClockSignal::Instance());
-            _dic.Add(bsp::HclkClockSignal::Instance().Name(), &bsp::HclkClockSignal::Instance());
-            _dic.Add(bsp::Pclk1ClockSignal::Instance().Name(), &bsp::Pclk1ClockSignal::Instance());
-            _dic.Add(bsp::Pclk2ClockSignal::Instance().Name(), &bsp::Pclk2ClockSignal::Instance());
-            _dic.Add(bsp::D1Pclk1ClockSignal::Instance().Name(), &bsp::D1Pclk1ClockSignal::Instance());
-            _dic.Add(bsp::D3Pclk1ClockSignal::Instance().Name(), &bsp::D3Pclk1ClockSignal::Instance());
-        }
+	class Initializer
+	{
+	public:
+		Initializer()
+		{
+			_dic.Add(bsp::SysclkClockSignal::Instance().Name(), &bsp::SysclkClockSignal::Instance());
+			_dic.Add(bsp::HclkClockSignal::Instance().Name(), &bsp::HclkClockSignal::Instance());
+			_dic.Add(bsp::Pclk1ClockSignal::Instance().Name(), &bsp::Pclk1ClockSignal::Instance());
+			_dic.Add(bsp::Pclk2ClockSignal::Instance().Name(), &bsp::Pclk2ClockSignal::Instance());
+			_dic.Add(bsp::D1Pclk1ClockSignal::Instance().Name(), &bsp::D1Pclk1ClockSignal::Instance());
+			_dic.Add(bsp::D3Pclk1ClockSignal::Instance().Name(), &bsp::D3Pclk1ClockSignal::Instance());
+		}
 
-        base::Dictionary<std::string, bsp::IClockSignal *> _dic{};
-    };
+		base::Dictionary<std::string, bsp::IClockSignal *> _dic{};
+	};
 
-    /// @brief 单例获取器
-    class Getter :
-        public bsp::TaskSingletonGetter<Initializer>
-    {
-    public:
-        std::unique_ptr<Initializer> Create() override
-        {
-            return std::unique_ptr<Initializer>{new Initializer{}};
-        }
-    };
+	/// @brief 单例获取器
+	class Getter :
+		public bsp::TaskSingletonGetter<Initializer>
+	{
+	public:
+		std::unique_ptr<Initializer> Create() override
+		{
+			return std::unique_ptr<Initializer>{new Initializer{}};
+		}
+	};
 } // namespace
 
 base::IDictionary<std::string, bsp::IClockSignal *> const &bsp::di::clock::ClockSignalCollection()
 {
-    Getter g;
-    return g.Instance()._dic;
+	Getter g;
+	return g.Instance()._dic;
 }
