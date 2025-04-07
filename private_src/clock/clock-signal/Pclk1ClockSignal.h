0342dc257a39115576a221455a6b3854aec9949a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-07 19:22:31 +0800

    _pclk2_hjc_init

--- private_src/clock/clock-signal/Pclk1ClockSignal.h
+++ private_src/clock/clock-signal/Pclk1ClockSignal.h
@@ -2,28 +2,27 @@
 #include <base/define.h>
 #include <bsp-interface/clock/IClockSignal.h>
 #include <bsp-interface/di/interrupt.h>
-#include <bsp-interface/TaskSingletonGetter.h>
 #include <hal.h>
 
 namespace bsp
 {
-    class Pclk1ClockSignal :
-        public bsp::IClockSignal
-    {
-    public:
-        static_function Pclk1ClockSignal &Instance();
+	class Pclk1ClockSignal :
+		public bsp::IClockSignal
+	{
+	public:
+		static_function Pclk1ClockSignal &Instance();
 
-        /// @brief 时钟信号的名称。
-        /// @return
-        virtual std::string Name() const override;
+		/// @brief 时钟信号的名称。
+		/// @return
+		virtual std::string Name() const override;
 
-        /// @brief 时钟信号的频率
-        /// @return
-        virtual base::MHz Frequency() const override;
+		/// @brief 时钟信号的频率
+		/// @return
+		virtual base::MHz Frequency() const override;
 
-        /// @brief 打开时钟信号。
-        /// @note 有的时钟信号只有输入分频，没有输出分频，就使用本重载。
-        /// @param input_division_factor
-        virtual void Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor) override;
-    };
+		/// @brief 打开时钟信号。
+		/// @note 有的时钟信号只有输入分频，没有输出分频，就使用本重载。
+		/// @param input_division_factor
+		virtual void Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor) override;
+	};
 } // namespace bsp
--- private_src/clock/clock-signal/Pclk2ClockSignal.cpp
+++ private_src/clock/clock-signal/Pclk2ClockSignal.cpp
@@ -1,76 +1,81 @@
 #include "Pclk2ClockSignal.h"
 #include <base/unit/Hz.h>
 
+namespace
+{
+	class Init
+	{
+	public:
+		Init()
+		{
+			bsp::Pclk2ClockSignal::Instance();
+		}
+	};
+
+	Init volatile _pclk2_hjc_init{};
+
+} // namespace
+
 bsp::Pclk2ClockSignal &bsp::Pclk2ClockSignal::Instance()
 {
-    class Getter :
-        public bsp::TaskSingletonGetter<Pclk2ClockSignal>
-    {
-    public:
-        std::unique_ptr<Pclk2ClockSignal> Create() override
-        {
-            return std::unique_ptr<Pclk2ClockSignal>{new Pclk2ClockSignal{}};
-        }
-    };
-
-    Getter g;
-    return g.Instance();
+	static Pclk2ClockSignal o{};
+	return o;
 }
 
 std::string bsp::Pclk2ClockSignal::Name() const
 {
-    return "pclk2";
+	return "pclk2";
 }
 
 base::MHz bsp::Pclk2ClockSignal::Frequency() const
 {
-    uint32_t value = HAL_RCC_GetPCLK2Freq();
-    return base::MHz{base::Hz{value}};
+	uint32_t value = HAL_RCC_GetPCLK2Freq();
+	return base::MHz{base::Hz{value}};
 }
 
 void bsp::Pclk2ClockSignal::Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor)
 {
-    RCC_ClkInitTypeDef def{};
-    def.ClockType = RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitTypeDef def{};
+	def.ClockType = RCC_CLOCKTYPE_PCLK2;
 
-    switch (input_division_factor.Value())
-    {
-    case 1:
-        {
-            def.APB2CLKDivider = RCC_APB2_DIV1;
-            break;
-        }
-    case 2:
-        {
-            def.APB2CLKDivider = RCC_APB2_DIV2;
-            break;
-        }
-    case 4:
-        {
-            def.APB2CLKDivider = RCC_APB2_DIV4;
-            break;
-        }
-    case 8:
-        {
-            def.APB2CLKDivider = RCC_APB2_DIV8;
-            break;
-        }
-    case 16:
-        {
-            def.APB2CLKDivider = RCC_APB2_DIV16;
-            break;
-        }
-    default:
-        {
-            throw std::invalid_argument{"不支持此分频"};
-        }
-    }
+	switch (input_division_factor.Value())
+	{
+	case 1:
+		{
+			def.APB2CLKDivider = RCC_APB2_DIV1;
+			break;
+		}
+	case 2:
+		{
+			def.APB2CLKDivider = RCC_APB2_DIV2;
+			break;
+		}
+	case 4:
+		{
+			def.APB2CLKDivider = RCC_APB2_DIV4;
+			break;
+		}
+	case 8:
+		{
+			def.APB2CLKDivider = RCC_APB2_DIV8;
+			break;
+		}
+	case 16:
+		{
+			def.APB2CLKDivider = RCC_APB2_DIV16;
+			break;
+		}
+	default:
+		{
+			throw std::invalid_argument{"不支持此分频"};
+		}
+	}
 
-    HAL_StatusTypeDef ret = HAL_RCC_ClockConfig(&def,
-                                                FLASH_LATENCY_2);
+	HAL_StatusTypeDef ret = HAL_RCC_ClockConfig(&def,
+												FLASH_LATENCY_2);
 
-    if (ret != HAL_StatusTypeDef::HAL_OK)
-    {
-        throw std::runtime_error{"时钟信号配置失败"};
-    }
+	if (ret != HAL_StatusTypeDef::HAL_OK)
+	{
+		throw std::runtime_error{"时钟信号配置失败"};
+	}
 }
