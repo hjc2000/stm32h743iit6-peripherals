cac4a78a1aab9dec187d6f72529cb5c23b734c43
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-09 00:27:05 +0800

    override

--- private_src/dma/dma-stream/Dma1Stream0.h
+++ private_src/dma/dma-stream/Dma1Stream0.h
@@ -4,75 +4,75 @@
 
 namespace bsp
 {
-    class Dma1Stream0 :
-        public bsp::DmaStream
-    {
-    private:
-        Dma1Stream0() = default;
+	class Dma1Stream0 :
+		public bsp::DmaStream
+	{
+	private:
+		Dma1Stream0() = default;
 
-        bool _is_open = false;
+		bool _is_open = false;
 
-    public:
-        static_function Dma1Stream0 &Instance();
+	public:
+		static_function Dma1Stream0 &Instance();
 
-        std::string Name() const override;
+		std::string Name() const override;
 
-        /// @brief 打开 DMA，模式为将数据从外设拷贝到内存。
-        /// @param parent DMA 要被连接到的父设备的句柄。例如对于 HAL 库的 USART，就是 UART_HandleTypeDef。
-        /// @param peripheral_increment
-        /// @param memory_increment
-        /// @param peripheral_data_alignment
-        /// @param memory_data_alignment
-        /// @param priority
-        /// @param request
-        virtual void OpenAsPeripheralToMemoryMode(void *parent,
-                                                  bsp::dma::property::PeripheralIncrement const &peripheral_increment,
-                                                  bsp::dma::property::MemoryIncrement const &memory_increment,
-                                                  bsp::dma::property::PeripheralDataAlignment const &peripheral_data_alignment,
-                                                  bsp::dma::property::MemoryDataAlignment const &memory_data_alignment,
-                                                  bsp::dma::property::Priority priority,
-                                                  std::string const &request);
+		/// @brief 打开 DMA，模式为将数据从外设拷贝到内存。
+		/// @param parent DMA 要被连接到的父设备的句柄。例如对于 HAL 库的 USART，就是 UART_HandleTypeDef。
+		/// @param peripheral_increment
+		/// @param memory_increment
+		/// @param peripheral_data_alignment
+		/// @param memory_data_alignment
+		/// @param priority
+		/// @param request
+		virtual void OpenAsPeripheralToMemoryMode(void *parent,
+												  bsp::dma::property::PeripheralIncrement const &peripheral_increment,
+												  bsp::dma::property::MemoryIncrement const &memory_increment,
+												  bsp::dma::property::PeripheralDataAlignment const &peripheral_data_alignment,
+												  bsp::dma::property::MemoryDataAlignment const &memory_data_alignment,
+												  bsp::dma::property::Priority priority,
+												  std::string const &request) override;
 
-        /// @brief 打开 DMA，模式为将数据从内存拷贝到外设。
-        /// @param parent DMA 要被连接到的父设备的句柄。例如对于 HAL 库的 USART，就是 UART_HandleTypeDef。
-        /// @param peripheral_increment
-        /// @param memory_increment
-        /// @param peripheral_data_alignment
-        /// @param memory_data_alignment
-        /// @param priority
-        /// @param request
-        virtual void OpenAsMemoryToPeripheralMode(void *parent,
-                                                  bsp::dma::property::PeripheralIncrement const &peripheral_increment,
-                                                  bsp::dma::property::MemoryIncrement const &memory_increment,
-                                                  bsp::dma::property::PeripheralDataAlignment const &peripheral_data_alignment,
-                                                  bsp::dma::property::MemoryDataAlignment const &memory_data_alignment,
-                                                  bsp::dma::property::Priority priority,
-                                                  std::string const &request);
+		/// @brief 打开 DMA，模式为将数据从内存拷贝到外设。
+		/// @param parent DMA 要被连接到的父设备的句柄。例如对于 HAL 库的 USART，就是 UART_HandleTypeDef。
+		/// @param peripheral_increment
+		/// @param memory_increment
+		/// @param peripheral_data_alignment
+		/// @param memory_data_alignment
+		/// @param priority
+		/// @param request
+		virtual void OpenAsMemoryToPeripheralMode(void *parent,
+												  bsp::dma::property::PeripheralIncrement const &peripheral_increment,
+												  bsp::dma::property::MemoryIncrement const &memory_increment,
+												  bsp::dma::property::PeripheralDataAlignment const &peripheral_data_alignment,
+												  bsp::dma::property::MemoryDataAlignment const &memory_data_alignment,
+												  bsp::dma::property::Priority priority,
+												  std::string const &request) override;
 
-        /// @brief 打开 DMA，模式为将数据从内存拷贝到内存。
-        /// @param parent DMA 要被连接到的父设备的句柄。例如对于 HAL 库的 USART，就是 UART_HandleTypeDef。
-        /// @param peripheral_increment
-        /// @param memory_increment
-        /// @param peripheral_data_alignment
-        /// @param memory_data_alignment
-        /// @param priority
-        /// @param request
-        virtual void OpenAsMomoryToMemoryMode(void *parent,
-                                              bsp::dma::property::PeripheralIncrement const &peripheral_increment,
-                                              bsp::dma::property::MemoryIncrement const &memory_increment,
-                                              bsp::dma::property::PeripheralDataAlignment const &peripheral_data_alignment,
-                                              bsp::dma::property::MemoryDataAlignment const &memory_data_alignment,
-                                              bsp::dma::property::Priority priority,
-                                              std::string const &request);
+		/// @brief 打开 DMA，模式为将数据从内存拷贝到内存。
+		/// @param parent DMA 要被连接到的父设备的句柄。例如对于 HAL 库的 USART，就是 UART_HandleTypeDef。
+		/// @param peripheral_increment
+		/// @param memory_increment
+		/// @param peripheral_data_alignment
+		/// @param memory_data_alignment
+		/// @param priority
+		/// @param request
+		virtual void OpenAsMomoryToMemoryMode(void *parent,
+											  bsp::dma::property::PeripheralIncrement const &peripheral_increment,
+											  bsp::dma::property::MemoryIncrement const &memory_increment,
+											  bsp::dma::property::PeripheralDataAlignment const &peripheral_data_alignment,
+											  bsp::dma::property::MemoryDataAlignment const &memory_data_alignment,
+											  bsp::dma::property::Priority priority,
+											  std::string const &request) override;
 
-        bool IsOpen() const override
-        {
-            return _is_open;
-        }
+		virtual bool IsOpen() const override
+		{
+			return _is_open;
+		}
 
-        void Close() override
-        {
-            _is_open = false;
-        }
-    };
+		virtual void Close() override
+		{
+			_is_open = false;
+		}
+	};
 } // namespace bsp
