717d9ebac9110ebca0c2aea38ca4dd196f3ff56d
hjc2000 <hjc.666@foxmail.com>
2025-04-16 10:12:16 +0800

    UsageStateManager

--- private_src/gpio/gpio_pin_handle.cpp
+++ private_src/gpio/gpio_pin_handle.cpp
@@ -1,216 +1,10 @@
 #include "gpio_pin_handle.h"
-#include "base/define.h"
-#include "base/LockGuard.h"
 #include "base/peripheral/gpio/gpio_parameter.h"
 #include "base/string/define.h"
-#include "base/task/IMutex.h"
 #include "bsp-interface/di/interrupt.h"
 #include "stm32h7xx_hal_gpio.h"
-#include <array>
-#include <bitset>
 #include <cstdint>
-#include <memory>
 #include <stdexcept>
-#include <string>
-
-namespace
-{
-	constexpr int PortCount()
-	{
-		return static_cast<int>(base::gpio::PortEnum::PortP) + 1;
-	}
-
-	std::string PinName(base::gpio::PortEnum port, uint32_t pin)
-	{
-		return base::to_string(port) + std::to_string(pin);
-	}
-
-	/* #region UsageStateManager */
-
-	class UsageStateManager
-	{
-	private:
-		UsageStateManager() = default;
-
-		std::array<std::bitset<16>, PortCount()> _states{};
-		std::shared_ptr<base::IMutex> _lock = base::CreateIMutex();
-
-		void CheckUsage(base::gpio::PortEnum port, uint32_t pin)
-		{
-			if (_states[static_cast<int>(port)][pin])
-			{
-				throw std::runtime_error{CODE_POS_STR + PinName(port, pin) + "已经占用了，无法再次占用。"};
-			}
-		}
-
-	public:
-		static UsageStateManager &Instance()
-		{
-			static UsageStateManager o;
-			return o;
-		}
-
-		void SetAsUsed(base::gpio::PortEnum port, uint32_t pin)
-		{
-			if (static_cast<int>(port) >= PortCount())
-			{
-				throw std::invalid_argument{CODE_POS_STR};
-			}
-
-			CheckUsage(port, pin);
-			base::LockGuard g{*_lock};
-			CheckUsage(port, pin);
-
-			_states[static_cast<int>(port)][pin] = true;
-		}
-
-		void SetAsUnused(base::gpio::PortEnum port, uint32_t pin)
-		{
-			if (static_cast<int>(port) >= PortCount())
-			{
-				throw std::invalid_argument{CODE_POS_STR};
-			}
-
-			base::LockGuard g{*_lock};
-			_states[static_cast<int>(port)][pin] = false;
-		}
-	};
-
-	PREINIT(UsageStateManager::Instance)
-
-	/* #endregion */
-
-	///
-	/// @brief 将端口枚举转为 HAL 库的端口实例指针。
-	///
-	/// @param value
-	/// @return constexpr GPIO_TypeDef*
-	///
-	constexpr GPIO_TypeDef *ToPort(base::gpio::PortEnum value)
-	{
-		switch (value)
-		{
-		case base::gpio::PortEnum::PortA:
-			{
-				return GPIOA;
-			}
-		case base::gpio::PortEnum::PortB:
-			{
-				return GPIOB;
-			}
-		case base::gpio::PortEnum::PortC:
-			{
-				return GPIOC;
-			}
-		case base::gpio::PortEnum::PortD:
-			{
-				return GPIOD;
-			}
-		case base::gpio::PortEnum::PortE:
-			{
-				return GPIOE;
-			}
-		case base::gpio::PortEnum::PortF:
-			{
-				return GPIOF;
-			}
-		case base::gpio::PortEnum::PortG:
-			{
-				return GPIOG;
-			}
-		case base::gpio::PortEnum::PortH:
-			{
-				return GPIOH;
-			}
-		default:
-			{
-				throw std::invalid_argument{CODE_POS_STR};
-			}
-		}
-	}
-
-	///
-	/// @brief 将引脚号转为 HAL 库的引脚宏定义。
-	///
-	/// @param pin
-	/// @return constexpr uint32_t
-	///
-	constexpr uint32_t ToPinDefineValue(uint32_t pin)
-	{
-		switch (pin)
-		{
-		case 0:
-			{
-				return GPIO_PIN_0;
-			}
-		case 1:
-			{
-				return GPIO_PIN_1;
-			}
-		case 2:
-			{
-				return GPIO_PIN_2;
-			}
-		case 3:
-			{
-				return GPIO_PIN_3;
-			}
-		case 4:
-			{
-				return GPIO_PIN_4;
-			}
-		case 5:
-			{
-				return GPIO_PIN_5;
-			}
-		case 6:
-			{
-				return GPIO_PIN_6;
-			}
-		case 7:
-			{
-				return GPIO_PIN_7;
-			}
-		case 8:
-			{
-				return GPIO_PIN_8;
-			}
-		case 9:
-			{
-				return GPIO_PIN_9;
-			}
-		case 10:
-			{
-				return GPIO_PIN_10;
-			}
-		case 11:
-			{
-				return GPIO_PIN_11;
-			}
-		case 12:
-			{
-				return GPIO_PIN_12;
-			}
-		case 13:
-			{
-				return GPIO_PIN_13;
-			}
-		case 14:
-			{
-				return GPIO_PIN_14;
-			}
-		case 15:
-			{
-				return GPIO_PIN_15;
-			}
-		default:
-			{
-				throw std::invalid_argument{CODE_POS_STR + "非法引脚号。"};
-			}
-		}
-	}
-
-} // namespace
 
 /* #region 初始化帮助方法 */
 
@@ -276,17 +70,12 @@ uint32_t base::gpio::gpio_pin_handle::get_alternate_function_define_value(base::
 /* #endregion */
 
 base::gpio::gpio_pin_handle::gpio_pin_handle(base::gpio::PortEnum port, uint32_t pin)
+	: _usage_state_manager(port, pin)
 {
 	_port_enum = port;
-	_port = ToPort(port);
+	_port = bsp::ToPort(port);
 	_pin = pin;
-	_pin_define = ToPinDefineValue(pin);
-	UsageStateManager::Instance().SetAsUsed(port, pin);
-}
-
-base::gpio::gpio_pin_handle::~gpio_pin_handle()
-{
-	UsageStateManager::Instance().SetAsUnused(_port_enum, _pin);
+	_pin_define = bsp::ToPinDefineValue(pin);
 }
 
 /* #region 初始化方法 */
--- private_src/gpio/gpio_pin_handle.h
+++ private_src/gpio/gpio_pin_handle.h
@@ -1,11 +1,202 @@
 #pragma once
+#include "base/LockGuard.h"
 #include "base/peripheral/gpio/gpio_handle.h" // IWYU pragma: export
+#include "base/task/IMutex.h"
 #include "hal.h"
+#include <array>
+#include <bitset>
 #include <cstdint>
+#include <memory>
+#include <string>
+
+namespace bsp
+{
+	constexpr int PortCount()
+	{
+		return static_cast<int>(base::gpio::PortEnum::PortP) + 1;
+	}
+
+	inline std::string PinName(base::gpio::PortEnum port, uint32_t pin)
+	{
+		return base::to_string(port) + std::to_string(pin);
+	}
+
+	///
+	/// @brief 将端口枚举转为 HAL 库的端口实例指针。
+	///
+	/// @param value
+	/// @return constexpr GPIO_TypeDef*
+	///
+	constexpr GPIO_TypeDef *ToPort(base::gpio::PortEnum value)
+	{
+		switch (value)
+		{
+		case base::gpio::PortEnum::PortA:
+			{
+				return GPIOA;
+			}
+		case base::gpio::PortEnum::PortB:
+			{
+				return GPIOB;
+			}
+		case base::gpio::PortEnum::PortC:
+			{
+				return GPIOC;
+			}
+		case base::gpio::PortEnum::PortD:
+			{
+				return GPIOD;
+			}
+		case base::gpio::PortEnum::PortE:
+			{
+				return GPIOE;
+			}
+		case base::gpio::PortEnum::PortF:
+			{
+				return GPIOF;
+			}
+		case base::gpio::PortEnum::PortG:
+			{
+				return GPIOG;
+			}
+		case base::gpio::PortEnum::PortH:
+			{
+				return GPIOH;
+			}
+		default:
+			{
+				throw std::invalid_argument{CODE_POS_STR};
+			}
+		}
+	}
+
+	///
+	/// @brief 将引脚号转为 HAL 库的引脚宏定义。
+	///
+	/// @param pin
+	/// @return constexpr uint32_t
+	///
+	constexpr uint32_t ToPinDefineValue(uint32_t pin)
+	{
+		switch (pin)
+		{
+		case 0:
+			{
+				return GPIO_PIN_0;
+			}
+		case 1:
+			{
+				return GPIO_PIN_1;
+			}
+		case 2:
+			{
+				return GPIO_PIN_2;
+			}
+		case 3:
+			{
+				return GPIO_PIN_3;
+			}
+		case 4:
+			{
+				return GPIO_PIN_4;
+			}
+		case 5:
+			{
+				return GPIO_PIN_5;
+			}
+		case 6:
+			{
+				return GPIO_PIN_6;
+			}
+		case 7:
+			{
+				return GPIO_PIN_7;
+			}
+		case 8:
+			{
+				return GPIO_PIN_8;
+			}
+		case 9:
+			{
+				return GPIO_PIN_9;
+			}
+		case 10:
+			{
+				return GPIO_PIN_10;
+			}
+		case 11:
+			{
+				return GPIO_PIN_11;
+			}
+		case 12:
+			{
+				return GPIO_PIN_12;
+			}
+		case 13:
+			{
+				return GPIO_PIN_13;
+			}
+		case 14:
+			{
+				return GPIO_PIN_14;
+			}
+		case 15:
+			{
+				return GPIO_PIN_15;
+			}
+		default:
+			{
+				throw std::invalid_argument{CODE_POS_STR + "非法引脚号。"};
+			}
+		}
+	}
+
+	class UsageStateManager
+	{
+	private:
+		inline static std::array<std::bitset<16>, PortCount()> _states{};
+		inline static std::shared_ptr<base::IMutex> _lock = base::CreateIMutex();
+
+		base::gpio::PortEnum _port{};
+		uint32_t _pin{};
+
+		void CheckUsage(base::gpio::PortEnum port, uint32_t pin)
+		{
+			if (_states[static_cast<int>(port)][pin])
+			{
+				throw std::runtime_error{CODE_POS_STR + PinName(port, pin) + "已经占用了，无法再次占用。"};
+			}
+		}
+
+	public:
+		UsageStateManager(base::gpio::PortEnum port, uint32_t pin)
+			: _port(port),
+			  _pin(pin)
+		{
+			if (static_cast<int>(port) >= PortCount())
+			{
+				throw std::invalid_argument{CODE_POS_STR};
+			}
+
+			CheckUsage(port, pin);
+			base::LockGuard g{*_lock};
+			CheckUsage(port, pin);
+
+			_states[static_cast<int>(port)][pin] = true;
+		}
+
+		~UsageStateManager()
+		{
+			base::LockGuard g{*_lock};
+			_states[static_cast<int>(_port)][_pin] = false;
+		}
+	};
+} // namespace bsp
 
 class base::gpio::gpio_pin_handle
 {
 private:
+	bsp::UsageStateManager _usage_state_manager;
 	base::gpio::PortEnum _port_enum{};
 	GPIO_TypeDef *_port = nullptr;
 	uint32_t _pin = 0;
@@ -21,7 +212,6 @@ private:
 
 public:
 	gpio_pin_handle(base::gpio::PortEnum port, uint32_t pin);
-	~gpio_pin_handle();
 
 	/* #region 初始化方法 */
 
