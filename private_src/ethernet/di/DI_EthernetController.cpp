dc07efad96d00cd97431f5b281dd5541217faf40
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-20 01:04:11 +0800

    ethernet_controller_handle

--- private_src/ethernet/di/DI_EthernetController.cpp
+++
@@ -1,9 +0,0 @@
-#include "ethernet_controller_handle.h"
-#include <bsp-interface/di/ethernet.h>
-
-/// @brief 以太网控制器。
-/// @return
-bsp::IEthernetController &bsp::di::ethernet::EthernetController()
-{
-	return base::ethernet::ethernet_controller_handle::Instance();
-}
--- private_src/ethernet/ethernet_controller_handle.h
+++ private_src/ethernet/ethernet_controller_handle.h
@@ -5,11 +5,9 @@
 #include "base/peripheral/ethernet/parameter.h"
 #include "base/task/IBinarySemaphore.h"
 #include "base/unit/Mbps.h"
-#include "bsp-interface/ethernet/IEthernetController.h"
 #include "hal.h"
 
-class base::ethernet::ethernet_controller_handle :
-	public bsp::IEthernetController
+class base::ethernet::ethernet_controller_handle
 {
 private:
 	class handle_context
@@ -45,12 +43,12 @@ public:
 
 	/// @brief 以太网控制器的名称。
 	/// @return
-	std::string Name() const override;
+	std::string Name() const;
 
 	/// @brief 获取本控制器绑定的 MAC 地址。
 	/// @note 要打开本控制器后本属性才有效。
 	/// @return
-	base::Mac Mac() const override;
+	base::Mac Mac() const;
 
 	/// @brief 打开以太网控制器。
 	/// @param interface_type 连接着 PHY 的接口类型。（使用的是 MII 还是 RMII）
@@ -58,36 +56,36 @@ public:
 	/// @param mac MAC 地址。
 	void Open(base::ethernet::InterfaceType interface_type,
 			  uint32_t phy_address,
-			  base::Mac const &mac) override;
+			  base::Mac const &mac);
 
 	/// @brief 读 PHY 的寄存器
 	/// @param register_index 寄存器索引。
 	/// @return
-	uint32_t ReadPHYRegister(uint32_t register_index) override;
+	uint32_t ReadPHYRegister(uint32_t register_index);
 
 	/// @brief 写 PHY 的寄存器。
 	/// @param register_index 寄存器索引。
 	/// @param value
-	void WritePHYRegister(uint32_t register_index, uint32_t value) override;
+	void WritePHYRegister(uint32_t register_index, uint32_t value);
 
 	/// @brief 启动以太网。
 	/// @param duplex_mode
 	/// @param speed
 	void Start(base::ethernet::DuplexMode duplex_mode,
-			   base::Mbps const &speed) override;
+			   base::Mbps const &speed);
 
 	/// @brief 发送。
 	/// @param spans
-	void Send(base::IEnumerable<base::ReadOnlySpan> const &spans) override;
+	void Send(base::IEnumerable<base::ReadOnlySpan> const &spans);
 
 	/// @brief 发送单个 span.
 	/// @note 默认实现是基于 void Send(base::IEnumerable<base::ReadOnlySpan> const &spans).
 	/// @param span
-	void Send(base::ReadOnlySpan const &span) override;
+	void Send(base::ReadOnlySpan const &span);
 
 	/// @brief 接收。
 	/// @note 因为接收后需要解析，而解析需要数据完整且连续，所以必须接收一整个完整的以太网帧，
 	/// 放到一个 span 中。
 	/// @return
-	base::ReadOnlySpan Receive() override;
+	base::ReadOnlySpan Receive();
 };
