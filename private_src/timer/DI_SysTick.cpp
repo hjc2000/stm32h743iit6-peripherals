7731db9af5ca4fb5a05ceaa6e29da107b4cf8bef
hjc2000 <hjc.666@foxmail.com>
2025-04-24 14:14:26 +0800

    systick

--- private_src/timer/DI_SysTick.cpp
+++
@@ -1,7 +0,0 @@
-#include <bsp-interface/di/systick.h>
-#include <SysTickClock.h>
-
-bsp::ISysTick &DI_SysTick()
-{
-    return bsp::SysTickClock::Instance();
-}
--- private_src/timer/systick.cpp
+++ private_src/timer/systick.cpp
@@ -1,53 +1,77 @@
 #include "base/peripheral/systick/systick.h"
-#include "base/define.h"
 #include "base/task/delay.h"
 #include "base/unit/Hz.h"
+#include "base/unit/MHz.h"
+#include "base/unit/Nanoseconds.h"
 #include "base/unit/Seconds.h"
 #include "bsp-interface/di/interrupt.h"
-#include "bsp-interface/timer/ISysTick.h"
+#include "hal.h"
+#include <chrono>
 
-///
-/// @brief 获取当前频率。
-///
-/// @return
-///
-base::MHz base::systick::frequency();
+namespace
+{
+	std::function<void()> _elapsed_handler{};
+	base::Nanoseconds _system_time_stamp{};
 
-///
-/// @brief 获取当前的计数值。
-///
-/// @note systick 被认为是减计数的。如果实际上是增计数的，可以将本函数实现为返回计数最大值减去
-/// 当前值，这样就虚拟成减计数的了。
-///
-/// @return
-///
-uint64_t base::systick::current_value();
+} // namespace
 
-///
-/// @brief 计数器的重装载值。
-///
-/// @note 计数器减计数到 0 后，下一个时钟周期会重新变成 reload_value 的值。
-///
-/// @return
-///
-uint64_t base::systick::reload_value();
+base::MHz base::systick::frequency()
+{
+	// stm32h743 不支持 8 分频。
+	return base::MHz{base::Hz{HAL_RCC_GetSysClockFreq()}};
+}
 
-///
-/// @brief 定时时间到处理函数。
-///
-/// @warning 实现者需要将本函数实现为线程安全和可重入。
-/// 	@note 最简单的方法就是在本函数的操作期间禁用全局中断。
-/// 	@note 最佳实践是只禁用 systick 的中断。
-///
-/// @warning 不要在中断函数中调用本函数。永远只能在主程序中调用本函数。
-///
-/// @param func
-///
-void base::systick::set_elapsed_handler(std::function<void()> func);
+uint64_t base::systick::current_value()
+{
+	uint32_t masked = SysTick->VAL & SysTick_VAL_CURRENT_Msk;
+	return masked >> SysTick_VAL_CURRENT_Pos;
+}
+
+uint64_t base::systick::reload_value()
+{
+	uint32_t masked = SysTick->LOAD & SysTick_LOAD_RELOAD_Msk;
+	return masked >> SysTick_LOAD_RELOAD_Pos;
+}
+
+void base::systick::set_elapsed_handler(std::function<void()> func)
+{
+	bsp::di::interrupt::GlobalInterruptGuard g;
+	_elapsed_handler = func;
+}
 
 ///
 /// @brief 系统时间戳。
 ///
 /// @return
 ///
-std::chrono::nanoseconds base::systick::system_time_stamp();
+base::Nanoseconds base::systick::system_time_stamp()
+{
+	return _system_time_stamp;
+}
+
+extern "C"
+{
+	/// @brief 重写 __weak 的 HAL_Delay 函数
+	/// @param ms 要延时的毫秒数。
+	void HAL_Delay(uint32_t ms)
+	{
+		base::task::Delay(std::chrono::milliseconds{ms});
+	}
+
+	void SysTick_Handler()
+	{
+		// 读取一下 CTRL，SysTick 的溢出标志位会自动清除。
+		SysTick->CTRL;
+		HAL_IncTick();
+
+		{
+			base::Nanoseconds tick_period{base::systick::frequency()};
+			_system_time_stamp += tick_period;
+		}
+
+		if (_elapsed_handler != nullptr)
+		{
+			_elapsed_handler();
+		}
+	}
+}
