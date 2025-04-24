605b0bc31b24dd96a603357560166dcd95882b2f
hjc2000 <hjc.666@foxmail.com>
2025-04-24 16:00:16 +0800

    注释

--- private_src/cache.cpp
+++ private_src/cache.cpp
@@ -24,12 +24,6 @@ void base::cache::invalidate_d_cache(void *address, size_t size)
 	SCB_InvalidateDCache_by_Addr(address, size);
 }
 
-///
-/// @brief 清除 D-Cache。
-///
-/// @note 即将原本从内存中读取到 D-Cache 中，并且已经修改过的 D-Cache 的数据写入
-/// 内存中而不是仅仅停留在 D-Cache 中。
-///
 void base::cache::clean_d_cache()
 {
 	SCB_CleanDCache();
