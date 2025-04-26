7e7d1e8102ddafc65fb0e337dd1a087b14ccc620
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-26 19:24:19 +0800

    删除target_import_task(${ProjectName} PUBLIC)

--- project.cmake
+++ project.cmake
@@ -3,5 +3,4 @@ target_import_src(${ProjectName})
 
 target_import_base(${ProjectName} PUBLIC)
 target_import_bsp_interface(${ProjectName} PUBLIC)
-target_import_task(${ProjectName} PUBLIC)
 target_import_stm32h743iit6_hal(${ProjectName} PRIVATE)
