9eeefaed01ab6a01c69854dd08c2a814cf798d4e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-28 00:21:26 +0800

    删除一些没用到的旧版GPIO单例

--- private_src/gpio/di/DI_GpioPinCollection.cpp
+++ private_src/gpio/di/DI_GpioPinCollection.cpp
@@ -1,56 +1,10 @@
 #include "base/container/Dictionary.h"
 #include "base/define.h"
 
-/* #region PA */
-
-#include "PA/GpioPinPA0.h"
-#include "PA/GpioPinPA1.h"
-
-/* #endregion */
-
 /* #region PB */
 
 #include "PB/GpioPinPB12.h"
 #include "PB/GpioPinPB5.h"
-
-/* #endregion */
-
-/* #region PC */
-
-#include "PC/GpioPinPC1.h"
-#include "PC/GpioPinPC5.h"
-
-/* #endregion */
-
-/* #region PD */
-
-#include "PD/GpioPinPD11.h"
-#include "PD/GpioPinPD4.h"
-#include "PD/GpioPinPD5.h"
-
-/* #endregion */
-
-/* #region PE */
-
-#include "PE/GpioPinPE2.h"
-#include "PE/GpioPinPE3.h"
-#include "PE/GpioPinPE4.h"
-#include "PE/GpioPinPE5.h"
-
-/* #endregion */
-
-/* #region PG */
-
-#include "PG/GpioPinPG12.h"
-
-/* #endregion */
-
-/* #region PH */
-
-#include "PH/GpioPinPH2.h"
-#include "PH/GpioPinPH3.h"
-#include "PH/GpioPinPH4.h"
-#include "PH/GpioPinPH5.h"
 
 /* #endregion */
 
@@ -67,56 +21,10 @@ namespace
 	public:
 		DictionaryProvider()
 		{
-			/* #region PA */
-
-			AddPin(bsp::GpioPinPA0::Instance());
-			AddPin(bsp::GpioPinPA1::Instance());
-
-			/* #endregion */
-
 			/* #region PB */
 
 			AddPin(bsp::GpioPinPB5::Instance());
 			AddPin(bsp::GpioPinPB12::Instance());
-
-			/* #endregion */
-
-			/* #region PC */
-
-			AddPin(bsp::GpioPinPC1::Instance());
-			AddPin(bsp::GpioPinPC5::Instance());
-
-			/* #endregion */
-
-			/* #region PD */
-
-			AddPin(bsp::GpioPinPD4::Instance());
-			AddPin(bsp::GpioPinPD5::Instance());
-			AddPin(bsp::GpioPinPD11::Instance());
-
-			/* #endregion */
-
-			/* #region PE */
-
-			AddPin(bsp::GpioPinPE2::Instance());
-			AddPin(bsp::GpioPinPE3::Instance());
-			AddPin(bsp::GpioPinPE4::Instance());
-			AddPin(bsp::GpioPinPE5::Instance());
-
-			/* #endregion */
-
-			/* #region PG */
-
-			AddPin(bsp::GpioPinPG12::Instance());
-
-			/* #endregion */
-
-			/* #region PH */
-
-			AddPin(bsp::GpioPinPH2::Instance());
-			AddPin(bsp::GpioPinPH3::Instance());
-			AddPin(bsp::GpioPinPH4::Instance());
-			AddPin(bsp::GpioPinPH5::Instance());
 
 			/* #endregion */
 		}
