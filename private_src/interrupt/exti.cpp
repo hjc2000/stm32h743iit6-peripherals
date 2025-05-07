caea2296d89e6990a7823fc8f31adb7d31e8d624
hjc2000 <hjc.666@foxmail.com>
2025-05-07 09:45:48 +0800

    exti

--- private_src/interrupt/exti.cpp
+++ private_src/interrupt/exti.cpp
@@ -1,15 +1,11 @@
 #include "base/embedded/interrupt/exti.h"
 #include "base/embedded/interrupt/interrupt.h"
+#include "base/string/define.h"
 #include "hal.h"
 
 namespace
 {
-	std::function<void()> _on_exti0_interrupt;
-	std::function<void()> _on_exti1_interrupt;
-	std::function<void()> _on_exti2_interrupt;
-	std::function<void()> _on_exti3_interrupt;
-	std::function<void()> _on_exti4_interrupt;
-	std::function<void()> _on_exti13_interrupt;
+	std::function<void()> _exti_interrupt_callback_handlers[16]{};
 
 } // namespace
 
@@ -20,48 +16,64 @@ void base::exti::register_callback(int32_t line_id, std::function<void()> const 
 	case 0:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI0_IRQn));
-			_on_exti0_interrupt = callback;
+			_exti_interrupt_callback_handlers[0] = callback;
 			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI0_IRQn), 4);
 			break;
 		}
 	case 1:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI1_IRQn));
-			_on_exti1_interrupt = callback;
+			_exti_interrupt_callback_handlers[1] = callback;
 			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI1_IRQn), 4);
 			break;
 		}
 	case 2:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI2_IRQn));
-			_on_exti2_interrupt = callback;
+			_exti_interrupt_callback_handlers[2] = callback;
 			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI2_IRQn), 4);
 			break;
 		}
 	case 3:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI3_IRQn));
-			_on_exti3_interrupt = callback;
+			_exti_interrupt_callback_handlers[3] = callback;
 			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI3_IRQn), 4);
 			break;
 		}
 	case 4:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI4_IRQn));
-			_on_exti4_interrupt = callback;
+			_exti_interrupt_callback_handlers[4] = callback;
 			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI4_IRQn), 4);
 			break;
 		}
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		{
+			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI9_5_IRQn));
+			_exti_interrupt_callback_handlers[line_id] = callback;
+			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI9_5_IRQn), 4);
+			break;
+		}
+	case 10:
+	case 11:
+	case 12:
 	case 13:
+	case 14:
+	case 15:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI15_10_IRQn));
-			_on_exti13_interrupt = callback;
+			_exti_interrupt_callback_handlers[line_id] = callback;
 			base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI15_10_IRQn), 4);
 			break;
 		}
 	default:
 		{
-			throw std::invalid_argument{"pin 超出范围。"};
+			throw std::invalid_argument{CODE_POS_STR + "pin 超出范围。"};
 		}
 	}
 }
@@ -73,42 +85,57 @@ void base::exti::unregister_callback(int32_t line_id)
 	case 0:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI0_IRQn));
-			_on_exti0_interrupt = nullptr;
+			_exti_interrupt_callback_handlers[0] = nullptr;
 			break;
 		}
 	case 1:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI1_IRQn));
-			_on_exti1_interrupt = nullptr;
+			_exti_interrupt_callback_handlers[1] = nullptr;
 			break;
 		}
 	case 2:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI2_IRQn));
-			_on_exti2_interrupt = nullptr;
+			_exti_interrupt_callback_handlers[2] = nullptr;
 			break;
 		}
 	case 3:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI3_IRQn));
-			_on_exti3_interrupt = nullptr;
+			_exti_interrupt_callback_handlers[3] = nullptr;
 			break;
 		}
 	case 4:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI4_IRQn));
-			_on_exti4_interrupt = nullptr;
+			_exti_interrupt_callback_handlers[4] = nullptr;
 			break;
 		}
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		{
+			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI9_5_IRQn));
+			_exti_interrupt_callback_handlers[line_id] = nullptr;
+			break;
+		}
+	case 10:
+	case 11:
+	case 12:
 	case 13:
+	case 14:
+	case 15:
 		{
 			base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::EXTI15_10_IRQn));
-			_on_exti13_interrupt = nullptr;
+			_exti_interrupt_callback_handlers[line_id] = nullptr;
 			break;
 		}
 	default:
 		{
-			throw std::invalid_argument{"pin 超出范围。"};
+			throw std::invalid_argument{CODE_POS_STR + "pin 超出范围。"};
 		}
 	}
 }
@@ -172,11 +199,31 @@ extern "C"
 		}
 	}
 
+	void EXTI9_5_IRQHandler()
+	{
+		try
+		{
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_8);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_9);
+		}
+		catch (...)
+		{
+		}
+	}
+
 	void EXTI15_10_IRQHandler()
 	{
 		try
 		{
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_10);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_12);
 			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_14);
+			HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_15);
 		}
 		catch (...)
 		{
@@ -196,54 +243,144 @@ extern "C"
 		{
 		case GPIO_PIN_0:
 			{
-				if (_on_exti0_interrupt)
+				if (_exti_interrupt_callback_handlers[0])
 				{
-					_on_exti0_interrupt();
+					_exti_interrupt_callback_handlers[0]();
 				}
 
 				break;
 			}
 		case GPIO_PIN_1:
 			{
-				if (_on_exti1_interrupt)
+				if (_exti_interrupt_callback_handlers[1])
 				{
-					_on_exti1_interrupt();
+					_exti_interrupt_callback_handlers[1]();
 				}
 
 				break;
 			}
 		case GPIO_PIN_2:
 			{
-				if (_on_exti2_interrupt)
+				if (_exti_interrupt_callback_handlers[2])
 				{
-					_on_exti2_interrupt();
+					_exti_interrupt_callback_handlers[2]();
 				}
 
 				break;
 			}
 		case GPIO_PIN_3:
 			{
-				if (_on_exti3_interrupt)
+				if (_exti_interrupt_callback_handlers[3])
 				{
-					_on_exti3_interrupt();
+					_exti_interrupt_callback_handlers[3]();
 				}
 
 				break;
 			}
 		case GPIO_PIN_4:
 			{
-				if (_on_exti4_interrupt)
+				if (_exti_interrupt_callback_handlers[4])
 				{
-					_on_exti4_interrupt();
+					_exti_interrupt_callback_handlers[4]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_5:
+			{
+				if (_exti_interrupt_callback_handlers[5])
+				{
+					_exti_interrupt_callback_handlers[5]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_6:
+			{
+				if (_exti_interrupt_callback_handlers[6])
+				{
+					_exti_interrupt_callback_handlers[6]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_7:
+			{
+				if (_exti_interrupt_callback_handlers[7])
+				{
+					_exti_interrupt_callback_handlers[7]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_8:
+			{
+				if (_exti_interrupt_callback_handlers[8])
+				{
+					_exti_interrupt_callback_handlers[8]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_9:
+			{
+				if (_exti_interrupt_callback_handlers[9])
+				{
+					_exti_interrupt_callback_handlers[9]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_10:
+			{
+				if (_exti_interrupt_callback_handlers[10])
+				{
+					_exti_interrupt_callback_handlers[10]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_11:
+			{
+				if (_exti_interrupt_callback_handlers[11])
+				{
+					_exti_interrupt_callback_handlers[11]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_12:
+			{
+				if (_exti_interrupt_callback_handlers[12])
+				{
+					_exti_interrupt_callback_handlers[12]();
 				}
 
 				break;
 			}
 		case GPIO_PIN_13:
 			{
-				if (_on_exti13_interrupt)
+				if (_exti_interrupt_callback_handlers[13])
 				{
-					_on_exti13_interrupt();
+					_exti_interrupt_callback_handlers[13]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_14:
+			{
+				if (_exti_interrupt_callback_handlers[14])
+				{
+					_exti_interrupt_callback_handlers[14]();
+				}
+
+				break;
+			}
+		case GPIO_PIN_15:
+			{
+				if (_exti_interrupt_callback_handlers[15])
+				{
+					_exti_interrupt_callback_handlers[15]();
 				}
 
 				break;
