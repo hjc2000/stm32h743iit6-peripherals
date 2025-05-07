b7b0337bee7f48cde1b198eaec8846c8b09da681
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-07 22:45:56 +0800

    freertos_get_systic_clock_freq

--- private_src/freertos_port.cpp
+++ private_src/freertos_port.cpp
@@ -4,10 +4,15 @@ extern "C"
 {
 	uint32_t freertos_get_systic_clock_freq(uint8_t sync_to_cpu)
 	{
-		/* 无论 sync_to_cpu 是什么，都应该返回系统时钟频率，因为 stm32h743 不支持让
-		 * SysTick 与 CPU 不同频率。
-		 */
 		uint32_t freq = HAL_RCC_GetSysClockFreq();
+		if (!sync_to_cpu)
+		{
+			// 这里不能检查 SysTick->CTRL 的 bit2 来决定返回 HCLK 的频率还是返回 HCLK / 8,
+			// 因为 freertos 调用本函数的时候还没设置 SysTick->CTRL 的 bit2, 调用完后会
+			// 设置 SysTick->CTRL 的 bit2.
+			freq /= 8;
+		}
+
 		return freq;
 	}
 }
