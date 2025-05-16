e8dfa2bf16dcb61f8661925bd89e79bf90fe1082
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-17 01:22:25 +0800

    "gpio: 将端口、引脚和复用功能转换函数实现移至cpp文件，头文件仅保留声明"

--- private_src/gpio/gpio.h
+++ private_src/gpio/gpio.h
@@ -14,154 +14,19 @@ namespace bsp
 		/// @brief 将端口枚举转为 HAL 库的端口实例指针。
 		///
 		/// @param value
-		/// @return constexpr GPIO_TypeDef*
+		/// @return
 		///
-		constexpr GPIO_TypeDef *to_defined_value(base::gpio::PortEnum value)
-		{
-			switch (value)
-			{
-			case base::gpio::PortEnum::PortA:
-				{
-					return GPIOA;
-				}
-			case base::gpio::PortEnum::PortB:
-				{
-					return GPIOB;
-				}
-			case base::gpio::PortEnum::PortC:
-				{
-					return GPIOC;
-				}
-			case base::gpio::PortEnum::PortD:
-				{
-					return GPIOD;
-				}
-			case base::gpio::PortEnum::PortE:
-				{
-					return GPIOE;
-				}
-			case base::gpio::PortEnum::PortF:
-				{
-					return GPIOF;
-				}
-			case base::gpio::PortEnum::PortG:
-				{
-					return GPIOG;
-				}
-			case base::gpio::PortEnum::PortH:
-				{
-					return GPIOH;
-				}
-			default:
-				{
-					throw std::invalid_argument{CODE_POS_STR + "非法端口。"};
-				}
-			}
-		}
+		GPIO_TypeDef *to_defined_value(base::gpio::PortEnum value);
 
 		///
 		/// @brief 将引脚号转为 HAL 库的引脚宏定义。
 		///
 		/// @param pin
-		/// @return constexpr uint32_t
+		/// @return
 		///
-		constexpr uint32_t to_pin_define_value(uint32_t pin)
-		{
-			switch (pin)
-			{
-			case 0:
-				{
-					return GPIO_PIN_0;
-				}
-			case 1:
-				{
-					return GPIO_PIN_1;
-				}
-			case 2:
-				{
-					return GPIO_PIN_2;
-				}
-			case 3:
-				{
-					return GPIO_PIN_3;
-				}
-			case 4:
-				{
-					return GPIO_PIN_4;
-				}
-			case 5:
-				{
-					return GPIO_PIN_5;
-				}
-			case 6:
-				{
-					return GPIO_PIN_6;
-				}
-			case 7:
-				{
-					return GPIO_PIN_7;
-				}
-			case 8:
-				{
-					return GPIO_PIN_8;
-				}
-			case 9:
-				{
-					return GPIO_PIN_9;
-				}
-			case 10:
-				{
-					return GPIO_PIN_10;
-				}
-			case 11:
-				{
-					return GPIO_PIN_11;
-				}
-			case 12:
-				{
-					return GPIO_PIN_12;
-				}
-			case 13:
-				{
-					return GPIO_PIN_13;
-				}
-			case 14:
-				{
-					return GPIO_PIN_14;
-				}
-			case 15:
-				{
-					return GPIO_PIN_15;
-				}
-			default:
-				{
-					throw std::invalid_argument{CODE_POS_STR + "非法引脚号。"};
-				}
-			}
-		}
+		uint32_t to_pin_define_value(uint32_t pin);
 
-		constexpr uint32_t to_defined_value(base::gpio::AlternateFunction af)
-		{
-			switch (af)
-			{
-			case base::gpio::AlternateFunction::UART1:
-				{
-					return GPIO_AF7_USART1;
-				}
-			case base::gpio::AlternateFunction::SDRAM1:
-				{
-					return GPIO_AF12_FMC;
-				}
-			case base::gpio::AlternateFunction::TIMER3:
-				{
-					return GPIO_AF2_TIM3;
-				}
-			default:
-				{
-					throw std::invalid_argument{CODE_POS_STR + "非法复用功能。"};
-				}
-			}
-		}
+		uint32_t to_defined_value(base::gpio::AlternateFunction af);
 
 	} // namespace gpio
 } // namespace bsp
