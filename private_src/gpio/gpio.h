8224d3384950e627433e90192a83d2f6eaeb10b1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-17 01:17:41 +0800

    "gpio: 将端口和引脚转换函数移至gpio.h并统一命名风格，移除冗余实现"

--- private_src/gpio/gpio.h
+++ private_src/gpio/gpio.h
@@ -10,6 +10,136 @@ namespace bsp
 	{
 		void enable_clock(GPIO_TypeDef *port);
 
+		///
+		/// @brief 将端口枚举转为 HAL 库的端口实例指针。
+		///
+		/// @param value
+		/// @return constexpr GPIO_TypeDef*
+		///
+		constexpr GPIO_TypeDef *to_defined_value(base::gpio::PortEnum value)
+		{
+			switch (value)
+			{
+			case base::gpio::PortEnum::PortA:
+				{
+					return GPIOA;
+				}
+			case base::gpio::PortEnum::PortB:
+				{
+					return GPIOB;
+				}
+			case base::gpio::PortEnum::PortC:
+				{
+					return GPIOC;
+				}
+			case base::gpio::PortEnum::PortD:
+				{
+					return GPIOD;
+				}
+			case base::gpio::PortEnum::PortE:
+				{
+					return GPIOE;
+				}
+			case base::gpio::PortEnum::PortF:
+				{
+					return GPIOF;
+				}
+			case base::gpio::PortEnum::PortG:
+				{
+					return GPIOG;
+				}
+			case base::gpio::PortEnum::PortH:
+				{
+					return GPIOH;
+				}
+			default:
+				{
+					throw std::invalid_argument{CODE_POS_STR + "非法端口。"};
+				}
+			}
+		}
+
+		///
+		/// @brief 将引脚号转为 HAL 库的引脚宏定义。
+		///
+		/// @param pin
+		/// @return constexpr uint32_t
+		///
+		constexpr uint32_t to_pin_define_value(uint32_t pin)
+		{
+			switch (pin)
+			{
+			case 0:
+				{
+					return GPIO_PIN_0;
+				}
+			case 1:
+				{
+					return GPIO_PIN_1;
+				}
+			case 2:
+				{
+					return GPIO_PIN_2;
+				}
+			case 3:
+				{
+					return GPIO_PIN_3;
+				}
+			case 4:
+				{
+					return GPIO_PIN_4;
+				}
+			case 5:
+				{
+					return GPIO_PIN_5;
+				}
+			case 6:
+				{
+					return GPIO_PIN_6;
+				}
+			case 7:
+				{
+					return GPIO_PIN_7;
+				}
+			case 8:
+				{
+					return GPIO_PIN_8;
+				}
+			case 9:
+				{
+					return GPIO_PIN_9;
+				}
+			case 10:
+				{
+					return GPIO_PIN_10;
+				}
+			case 11:
+				{
+					return GPIO_PIN_11;
+				}
+			case 12:
+				{
+					return GPIO_PIN_12;
+				}
+			case 13:
+				{
+					return GPIO_PIN_13;
+				}
+			case 14:
+				{
+					return GPIO_PIN_14;
+				}
+			case 15:
+				{
+					return GPIO_PIN_15;
+				}
+			default:
+				{
+					throw std::invalid_argument{CODE_POS_STR + "非法引脚号。"};
+				}
+			}
+		}
+
 		constexpr uint32_t to_defined_value(base::gpio::AlternateFunction af)
 		{
 			switch (af)
--- private_src/gpio/gpio_pin_handle.h
+++ private_src/gpio/gpio_pin_handle.h
@@ -7,140 +7,6 @@
 #include "hal.h"
 #include <cstdint>
 
-namespace bsp
-{
-	///
-	/// @brief 将端口枚举转为 HAL 库的端口实例指针。
-	///
-	/// @param value
-	/// @return constexpr GPIO_TypeDef*
-	///
-	constexpr GPIO_TypeDef *ToPort(base::gpio::PortEnum value)
-	{
-		switch (value)
-		{
-		case base::gpio::PortEnum::PortA:
-			{
-				return GPIOA;
-			}
-		case base::gpio::PortEnum::PortB:
-			{
-				return GPIOB;
-			}
-		case base::gpio::PortEnum::PortC:
-			{
-				return GPIOC;
-			}
-		case base::gpio::PortEnum::PortD:
-			{
-				return GPIOD;
-			}
-		case base::gpio::PortEnum::PortE:
-			{
-				return GPIOE;
-			}
-		case base::gpio::PortEnum::PortF:
-			{
-				return GPIOF;
-			}
-		case base::gpio::PortEnum::PortG:
-			{
-				return GPIOG;
-			}
-		case base::gpio::PortEnum::PortH:
-			{
-				return GPIOH;
-			}
-		default:
-			{
-				throw std::invalid_argument{CODE_POS_STR};
-			}
-		}
-	}
-
-	///
-	/// @brief 将引脚号转为 HAL 库的引脚宏定义。
-	///
-	/// @param pin
-	/// @return constexpr uint32_t
-	///
-	constexpr uint32_t ToPinDefineValue(uint32_t pin)
-	{
-		switch (pin)
-		{
-		case 0:
-			{
-				return GPIO_PIN_0;
-			}
-		case 1:
-			{
-				return GPIO_PIN_1;
-			}
-		case 2:
-			{
-				return GPIO_PIN_2;
-			}
-		case 3:
-			{
-				return GPIO_PIN_3;
-			}
-		case 4:
-			{
-				return GPIO_PIN_4;
-			}
-		case 5:
-			{
-				return GPIO_PIN_5;
-			}
-		case 6:
-			{
-				return GPIO_PIN_6;
-			}
-		case 7:
-			{
-				return GPIO_PIN_7;
-			}
-		case 8:
-			{
-				return GPIO_PIN_8;
-			}
-		case 9:
-			{
-				return GPIO_PIN_9;
-			}
-		case 10:
-			{
-				return GPIO_PIN_10;
-			}
-		case 11:
-			{
-				return GPIO_PIN_11;
-			}
-		case 12:
-			{
-				return GPIO_PIN_12;
-			}
-		case 13:
-			{
-				return GPIO_PIN_13;
-			}
-		case 14:
-			{
-				return GPIO_PIN_14;
-			}
-		case 15:
-			{
-				return GPIO_PIN_15;
-			}
-		default:
-			{
-				throw std::invalid_argument{CODE_POS_STR + "非法引脚号。"};
-			}
-		}
-	}
-
-} // namespace bsp
-
 class base::gpio::gpio_pin_handle
 {
 private:
@@ -155,9 +21,9 @@ public:
 		: _usage_state_manager(port, pin)
 	{
 		_port_enum = port;
-		_port = bsp::ToPort(port);
+		_port = bsp::gpio::to_defined_value(port);
 		_pin = pin;
-		_pin_define = bsp::ToPinDefineValue(pin);
+		_pin_define = bsp::gpio::to_pin_define_value(pin);
 	}
 
 	/* #region 初始化方法 */
