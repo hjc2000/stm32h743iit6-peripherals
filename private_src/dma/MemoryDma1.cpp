af48f0a3b7b9d17ffc30c049a746132d8ea47cea
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-18 00:24:11 +0800

    重构MemoryDma1回调处理，引入handle_context结构体并实现DMA回调信号机制

--- private_src/dma/MemoryDma1.cpp
+++ private_src/dma/MemoryDma1.cpp
@@ -22,13 +22,34 @@ extern "C"
 	}
 }
 
+void bsp::MemoryDma1::InitializeCallback()
+{
+	_handle_context._handle.XferCpltCallback = [](DMA_HandleTypeDef *handle)
+	{
+		MemoryDma1 *self = reinterpret_cast<MemoryDma1 *>(handle);
+		self->OnCompleteCallback();
+	};
+
+	_handle_context._handle.XferErrorCallback = [](DMA_HandleTypeDef *handle)
+	{
+		MemoryDma1 *self = reinterpret_cast<MemoryDma1 *>(handle);
+		self->OnErrorCallback();
+	};
+
+	_handle_context._handle.XferAbortCallback = [](DMA_HandleTypeDef *handle)
+	{
+		MemoryDma1 *self = reinterpret_cast<MemoryDma1 *>(handle);
+		self->OnAbortCallback();
+	};
+}
+
 void bsp::MemoryDma1::InitializeInterrupt()
 {
 	base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::DMA1_Stream2_IRQn));
 
 	_dma1_stream2_isr = [this]()
 	{
-		HAL_DMA_IRQHandler(&_handle);
+		HAL_DMA_IRQHandler(&_handle_context._handle);
 	};
 
 	base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::DMA1_Stream2_IRQn), 10);
@@ -44,20 +65,21 @@ void bsp::MemoryDma1::Initialize()
 {
 	__HAL_RCC_DMA1_CLK_ENABLE();
 
-	_handle.Instance = DMA1_Stream2;
-	_handle.Init.Direction = DMA_MEMORY_TO_MEMORY;
-	_handle.Init.Request = DMA_REQUEST_MEM2MEM;
-	_handle.Init.Mode = DMA_NORMAL;
-	_handle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-	_handle.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
-	_handle.Init.MemBurst = DMA_MBURST_SINGLE;
-	_handle.Init.PeriphBurst = DMA_PBURST_SINGLE;
-	_handle.Init.PeriphInc = DMA_PINC_ENABLE;
-	_handle.Init.MemInc = DMA_MINC_ENABLE;
-	_handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-	_handle.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-	_handle.Init.Priority = DMA_PRIORITY_MEDIUM;
-	HAL_DMA_Init(&_handle);
+	_handle_context._handle.Instance = DMA1_Stream2;
+	_handle_context._handle.Init.Direction = DMA_MEMORY_TO_MEMORY;
+	_handle_context._handle.Init.Request = DMA_REQUEST_MEM2MEM;
+	_handle_context._handle.Init.Mode = DMA_NORMAL;
+	_handle_context._handle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+	_handle_context._handle.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
+	_handle_context._handle.Init.MemBurst = DMA_MBURST_SINGLE;
+	_handle_context._handle.Init.PeriphBurst = DMA_PBURST_SINGLE;
+	_handle_context._handle.Init.PeriphInc = DMA_PINC_ENABLE;
+	_handle_context._handle.Init.MemInc = DMA_MINC_ENABLE;
+	_handle_context._handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+	_handle_context._handle.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+	_handle_context._handle.Init.Priority = DMA_PRIORITY_MEDIUM;
+	HAL_DMA_Init(&_handle_context._handle);
 
+	InitializeCallback();
 	InitializeInterrupt();
 }
--- private_src/dma/MemoryDma1.h
+++ private_src/dma/MemoryDma1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/task/BinarySemaphore.h"
 #include "base/UsageStateManager.h"
 #include "hal.h" // IWYU pragma: keep
 #include "memory_dma_handle.h"
@@ -9,10 +10,39 @@ namespace bsp
 		public base::memory_dma::memory_dma_handle
 	{
 	private:
-		base::UsageStateManager<MemoryDma1> _usage_state_manager{};
-		DMA_HandleTypeDef _handle{};
+		class handle_context
+		{
+		public:
+			handle_context(MemoryDma1 *self)
+			{
+				_self = self;
+			}
 
+			DMA_HandleTypeDef _handle{};
+			MemoryDma1 *_self{};
+		};
+
+		base::UsageStateManager<MemoryDma1> _usage_state_manager{};
+		handle_context _handle_context{this};
+		base::task::BinarySemaphore _complete_signal{false};
+
+		void InitializeCallback();
 		void InitializeInterrupt();
+
+		void OnCompleteCallback()
+		{
+			_complete_signal.ReleaseFromIsr();
+		}
+
+		void OnErrorCallback()
+		{
+			_complete_signal.ReleaseFromIsr();
+		}
+
+		void OnAbortCallback()
+		{
+			_complete_signal.ReleaseFromIsr();
+		}
 
 	public:
 		~MemoryDma1();
