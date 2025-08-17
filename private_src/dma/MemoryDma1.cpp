69cdd3f39492f6b63e5de8fa837370d42b2dae2f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-17 23:55:15 +0800

    重构MemoryDma1实现，分离中断初始化逻辑并完善DMA1_Stream2中断处理

--- private_src/dma/MemoryDma1.cpp
+++ private_src/dma/MemoryDma1.cpp
@@ -1,1 +1,57 @@
 #include "MemoryDma1.h" // IWYU pragma: keep
+#include "base/embedded/interrupt/interrupt.h"
+#include <functional>
+
+namespace
+{
+	std::function<void()> _dma1_stream2_isr;
+
+}
+
+extern "C"
+{
+	void DMA1_Stream2_IRQHandler()
+	{
+		try
+		{
+			_dma1_stream2_isr();
+		}
+		catch (...)
+		{
+		}
+	}
+}
+
+void bsp::MemoryDma1::InitializeInterrupt()
+{
+	base::interrupt::disable_interrupt(static_cast<uint32_t>(IRQn_Type::DMA1_Stream2_IRQn));
+
+	_dma1_stream2_isr = [this]()
+	{
+		HAL_DMA_IRQHandler(&_handle);
+	};
+
+	base::interrupt::enable_interrupt(static_cast<uint32_t>(IRQn_Type::DMA1_Stream2_IRQn), 10);
+}
+
+void bsp::MemoryDma1::Initialize()
+{
+	__HAL_RCC_DMA1_CLK_ENABLE();
+
+	_handle.Instance = DMA1_Stream2;
+	_handle.Init.Direction = DMA_MEMORY_TO_MEMORY;
+	_handle.Init.Request = DMA_REQUEST_MEM2MEM;
+	_handle.Init.Mode = DMA_NORMAL;
+	_handle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+	_handle.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
+	_handle.Init.MemBurst = DMA_MBURST_SINGLE;
+	_handle.Init.PeriphBurst = DMA_PBURST_SINGLE;
+	_handle.Init.PeriphInc = DMA_PINC_ENABLE;
+	_handle.Init.MemInc = DMA_MINC_ENABLE;
+	_handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+	_handle.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+	_handle.Init.Priority = DMA_PRIORITY_MEDIUM;
+	HAL_DMA_Init(&_handle);
+
+	InitializeInterrupt();
+}
--- private_src/dma/MemoryDma1.h
+++ private_src/dma/MemoryDma1.h
@@ -12,26 +12,10 @@ namespace bsp
 		base::UsageStateManager<MemoryDma1> _usage_state_manager{};
 		DMA_HandleTypeDef _handle{};
 
-	public:
-		virtual void Initialize() override
-		{
-			__HAL_RCC_DMA1_CLK_ENABLE();
+		void InitializeInterrupt();
 
-			_handle.Instance = DMA1_Stream2;
-			_handle.Init.Direction = DMA_MEMORY_TO_MEMORY;
-			_handle.Init.Request = DMA_REQUEST_MEM2MEM;
-			_handle.Init.Mode = DMA_NORMAL;
-			_handle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-			_handle.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_FULL;
-			_handle.Init.MemBurst = DMA_MBURST_SINGLE;
-			_handle.Init.PeriphBurst = DMA_PBURST_SINGLE;
-			_handle.Init.PeriphInc = DMA_PINC_ENABLE;
-			_handle.Init.MemInc = DMA_MINC_ENABLE;
-			_handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-			_handle.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-			_handle.Init.Priority = DMA_PRIORITY_MEDIUM;
-			HAL_DMA_Init(&_handle);
-		}
+	public:
+		virtual void Initialize() override;
 
 		virtual void Copy(uint8_t const *begin,
 						  uint8_t const *end,
