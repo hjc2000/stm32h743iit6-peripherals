c70a88b9aa2737363a117842f9156246e51781e1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-18 00:49:28 +0800

    实现MemoryDma1的open接口，返回MemoryDma1实例

--- private_src/dma/memory_dma_handle.cpp
+++ private_src/dma/memory_dma_handle.cpp
@@ -1,6 +1,10 @@
 #include "memory_dma_handle.h" // IWYU pragma: keep
+#include "MemoryDma1.h"
 
-std::shared_ptr<base::memory_dma::memory_dma_handle> base::memory_dma::open(uint32_t id);
+std::shared_ptr<base::memory_dma::memory_dma_handle> base::memory_dma::open(uint32_t id)
+{
+	return std::shared_ptr<base::memory_dma::memory_dma_handle>{new bsp::MemoryDma1{}};
+}
 
 void base::memory_dma::initialize(base::memory_dma::memory_dma_handle &self)
 {
