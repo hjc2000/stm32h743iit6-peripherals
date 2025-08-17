b13af070a2d8c8b4f0c7034890499971d0155ae7
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-18 00:57:41 +0800

    为cache模块添加按地址清理和失效DCache的功能

--- private_src/cache.cpp
+++ private_src/cache.cpp
@@ -1,5 +1,6 @@
 #include "base/embedded/cache/cache.h"
 #include "hal.h"
+#include <cstdint>
 
 void base::cache::enable()
 {
@@ -29,7 +30,17 @@ void base::cache::clean_d_cache()
 	SCB_CleanDCache();
 }
 
+void base::cache::clean_d_cache(void *address, size_t size)
+{
+	SCB_CleanDCache_by_Addr(reinterpret_cast<uint32_t *>(address), size);
+}
+
 void base::cache::clean_and_invalidate_d_cache()
 {
 	SCB_CleanInvalidateDCache();
+}
+
+void base::cache::clean_and_invalidate_d_cache(void *address, size_t size)
+{
+	SCB_CleanInvalidateDCache_by_Addr(reinterpret_cast<uint32_t *>(address), size);
 }
