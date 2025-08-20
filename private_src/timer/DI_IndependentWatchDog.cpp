eeb9192273ced1722ddd9fca0c6ab016205e4d66
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-20 23:36:36 +0800

    移除独立看门狗相关实现代码，包括配置类、接口实现和依赖注入

--- private_src/timer/DI_IndependentWatchDog.cpp
+++
@@ -1,7 +0,0 @@
-#include <bsp-interface/di/independent-watch-dog.h>
-#include <IndependentWatchDog.h>
-
-bsp::IIndependentWatchDog &DI_IndependentWatchDog()
-{
-    return bsp::IndependentWatchDog::Instance();
-}
