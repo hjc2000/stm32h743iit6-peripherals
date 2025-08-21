29a305b8a56afd1d24378b6826e1df5f66499851
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-21 22:12:58 +0800

    重构以太网控制器1实现，移除GPIO初始化逻辑并添加msp_initialize_callback调用

--- private_src/ethernet/EthernetController1.cpp
+++ private_src/ethernet/EthernetController1.cpp
@@ -1,39 +1,15 @@
 #include "EthernetController1.h"
 #include "base/Console.h"
 #include "base/embedded/cache/cache.h"
+#include "base/embedded/ethernet/ethernet_controller_handle.h"
 #include "base/embedded/interrupt/interrupt.h"
 #include "bsp-interface/di/interrupt.h"
 #include "hal.h"
 
-#define ETH_CLK_GPIO_PORT GPIOA
-#define ETH_CLK_GPIO_PIN GPIO_PIN_1
-
-#define ETH_MDIO_GPIO_PORT GPIOA
-#define ETH_MDIO_GPIO_PIN GPIO_PIN_2
-
-#define ETH_CRS_GPIO_PORT GPIOA
-#define ETH_CRS_GPIO_PIN GPIO_PIN_7
-
-#define ETH_MDC_GPIO_PORT GPIOC
-#define ETH_MDC_GPIO_PIN GPIO_PIN_1
-
-#define ETH_RXD0_GPIO_PORT GPIOC
-#define ETH_RXD0_GPIO_PIN GPIO_PIN_4
-
-#define ETH_RXD1_GPIO_PORT GPIOC
-#define ETH_RXD1_GPIO_PIN GPIO_PIN_5
-
-#define ETH_TX_EN_GPIO_PORT GPIOB
-#define ETH_TX_EN_GPIO_PIN GPIO_PIN_11
-
-#define ETH_TXD0_GPIO_PORT GPIOG
-#define ETH_TXD0_GPIO_PIN GPIO_PIN_13
-
-#define ETH_TXD1_GPIO_PORT GPIOG
-#define ETH_TXD1_GPIO_PIN GPIO_PIN_14
-
 bsp::EthernetController1::EthernetController1()
 {
+	base::ethernet::msp_initialize_callback(1);
+
 	// MPU 设置
 	{
 		MPU_Region_InitTypeDef MPU_InitStruct{};
@@ -51,74 +27,6 @@ bsp::EthernetController1::EthernetController1()
 		MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
 		HAL_MPU_ConfigRegion(&MPU_InitStruct);
 		HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-	}
-
-	// GPIO 初始化
-	{
-		GPIO_InitTypeDef gpio_init_struct;
-
-		__HAL_RCC_GPIOA_CLK_ENABLE(); /* 开启ETH_CLK时钟 */
-		__HAL_RCC_GPIOA_CLK_ENABLE(); /* 开启ETH_MDIO时钟 */
-		__HAL_RCC_GPIOA_CLK_ENABLE(); /* 开启ETH_CRS时钟 */
-		__HAL_RCC_GPIOC_CLK_ENABLE(); /* 开启ETH_MDC时钟 */
-		__HAL_RCC_GPIOC_CLK_ENABLE(); /* 开启ETH_RXD0时钟 */
-		__HAL_RCC_GPIOC_CLK_ENABLE(); /* 开启ETH_RXD1时钟 */
-		__HAL_RCC_GPIOB_CLK_ENABLE(); /* 开启ETH_TX_EN时钟 */
-		__HAL_RCC_GPIOG_CLK_ENABLE(); /* 开启ETH_TXD0时钟 */
-		__HAL_RCC_GPIOG_CLK_ENABLE(); /* 开启ETH_TXD1时钟 */
-
-		/* Enable Ethernet clocks */
-		__HAL_RCC_ETH1MAC_CLK_ENABLE();
-		__HAL_RCC_ETH1TX_CLK_ENABLE();
-		__HAL_RCC_ETH1RX_CLK_ENABLE();
-
-		/* 网络引脚设置 RMII接口
-		 * ETH_MDIO -------------------------> PA2
-		 * ETH_MDC --------------------------> PC1
-		 * ETH_RMII_REF_CLK------------------> PA1
-		 * ETH_RMII_CRS_DV ------------------> PA7
-		 * ETH_RMII_RXD0 --------------------> PC4
-		 * ETH_RMII_RXD1 --------------------> PC5
-		 * ETH_RMII_TX_EN -------------------> PB11
-		 * ETH_RMII_TXD0 --------------------> PG13
-		 * ETH_RMII_TXD1 --------------------> PG14
-		 */
-
-		/* PA1,2,7 */
-		gpio_init_struct.Pin = ETH_CLK_GPIO_PIN;
-		gpio_init_struct.Mode = GPIO_MODE_AF_PP;             /* 推挽复用 */
-		gpio_init_struct.Pull = GPIO_NOPULL;                 /* 不带上下拉 */
-		gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;       /* 高速 */
-		gpio_init_struct.Alternate = GPIO_AF11_ETH;          /* 复用为ETH功能 */
-		HAL_GPIO_Init(ETH_CLK_GPIO_PORT, &gpio_init_struct); /* ETH_CLK引脚模式设置 */
-
-		gpio_init_struct.Pin = ETH_MDIO_GPIO_PIN;
-		HAL_GPIO_Init(ETH_MDIO_GPIO_PORT, &gpio_init_struct); /* ETH_MDIO引脚模式设置 */
-
-		gpio_init_struct.Pin = ETH_CRS_GPIO_PIN;
-		HAL_GPIO_Init(ETH_CRS_GPIO_PORT, &gpio_init_struct); /* ETH_CRS引脚模式设置 */
-
-		/* PC1 */
-		gpio_init_struct.Pin = ETH_MDC_GPIO_PIN;
-		HAL_GPIO_Init(ETH_MDC_GPIO_PORT, &gpio_init_struct); /* ETH_MDC初始化 */
-
-		/* PC4 */
-		gpio_init_struct.Pin = ETH_RXD0_GPIO_PIN;
-		HAL_GPIO_Init(ETH_RXD0_GPIO_PORT, &gpio_init_struct); /* ETH_RXD0初始化 */
-
-		/* PC5 */
-		gpio_init_struct.Pin = ETH_RXD1_GPIO_PIN;
-		HAL_GPIO_Init(ETH_RXD1_GPIO_PORT, &gpio_init_struct); /* ETH_RXD1初始化 */
-
-		/* PB11,PG13,PG14 */
-		gpio_init_struct.Pin = ETH_TX_EN_GPIO_PIN;
-		HAL_GPIO_Init(ETH_TX_EN_GPIO_PORT, &gpio_init_struct); /* ETH_TX_EN初始化 */
-
-		gpio_init_struct.Pin = ETH_TXD0_GPIO_PIN;
-		HAL_GPIO_Init(ETH_TXD0_GPIO_PORT, &gpio_init_struct); /* ETH_TXD0初始化 */
-
-		gpio_init_struct.Pin = ETH_TXD1_GPIO_PIN;
-		HAL_GPIO_Init(ETH_TXD1_GPIO_PORT, &gpio_init_struct); /* ETH_TXD1初始化 */
 	}
 
 	_sending_config.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
