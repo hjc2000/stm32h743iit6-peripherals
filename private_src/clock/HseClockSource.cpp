90530118ee443dd31ed33428ee566d0675d211af
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-21 23:02:20 +0800

    重构HSE时钟源实现，将配置逻辑移至cpp文件并清理头文件

---
+++ private_src/clock/HseClockSource.cpp
@@ -0,0 +1,47 @@
+#include "HseClockSource.h" // IWYU pragma: keep
+#include "hal.h"
+
+void bsp::HseClockSource::Configure()
+{
+	RCC_OscInitTypeDef def{};
+	def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	def.HSEState = RCC_HSE_ON;
+	HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
+
+	if (result != HAL_StatusTypeDef::HAL_OK)
+	{
+		throw std::runtime_error{"打开 hse 时钟源失败。"};
+	}
+
+	_frequency = base::unit::MHz{25};
+}
+
+void bsp::HseClockSource::ConfigureAsBypassMode(base::unit::MHz const &bypass_input_frequency)
+{
+	RCC_OscInitTypeDef def{};
+	def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	def.HSEState = RCC_HSE_BYPASS;
+
+	HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
+	if (result != HAL_StatusTypeDef::HAL_OK)
+	{
+		throw std::runtime_error{"设置 hse 时钟源为旁路失败。"};
+	}
+
+	_frequency = bypass_input_frequency;
+}
+
+void bsp::HseClockSource::TurnOff()
+{
+	RCC_OscInitTypeDef def{};
+	def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	def.HSEState = RCC_HSE_OFF;
+
+	HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
+	if (result != HAL_StatusTypeDef::HAL_OK)
+	{
+		throw std::runtime_error{"关闭 hse 时钟源失败。"};
+	}
+
+	_frequency = base::unit::MHz{0};
+}
--- private_src/clock/HseClockSource.h
+++ private_src/clock/HseClockSource.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "base/unit/MHz.h"
 #include "clock_source_handle.h"
-#include "hal.h"
 
 namespace bsp
 {
@@ -18,48 +17,11 @@ namespace bsp
 			return _frequency;
 		}
 
-		virtual void Configure() override
-		{
-			RCC_OscInitTypeDef def{};
-			def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-			def.HSEState = RCC_HSE_ON;
-			HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
-			if (result != HAL_StatusTypeDef::HAL_OK)
-			{
-				throw std::runtime_error{"打开 hse 时钟源失败。"};
-			}
+		virtual void Configure() override;
 
-			_frequency = base::unit::MHz{25};
-		}
+		virtual void ConfigureAsBypassMode(base::unit::MHz const &bypass_input_frequency) override;
 
-		virtual void ConfigureAsBypassMode(base::unit::MHz const &bypass_input_frequency) override
-		{
-			RCC_OscInitTypeDef def{};
-			def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-			def.HSEState = RCC_HSE_BYPASS;
-
-			HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
-			if (result != HAL_StatusTypeDef::HAL_OK)
-			{
-				throw std::runtime_error{"设置 hse 时钟源为旁路失败。"};
-			}
-
-			_frequency = bypass_input_frequency;
-		}
-
-		virtual void TurnOff() override
-		{
-			RCC_OscInitTypeDef def{};
-			def.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-			def.HSEState = RCC_HSE_OFF;
-
-			HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
-			if (result != HAL_StatusTypeDef::HAL_OK)
-			{
-				throw std::runtime_error{"关闭 hse 时钟源失败。"};
-			}
-
-			_frequency = base::unit::MHz{0};
-		}
+		virtual void TurnOff() override;
 	};
+
 } // namespace bsp
