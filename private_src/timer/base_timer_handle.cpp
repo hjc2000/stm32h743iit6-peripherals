544e1fbc9636bf4d8a69827f46c84faa869c806b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-21 23:07:40 +0800

    实现基础定时器句柄功能，添加基础定时器句柄类定义

---
+++ private_src/timer/base_timer_handle.cpp
@@ -0,0 +1,1 @@
+#include "base_timer_handle.h" // IWYU pragma: keep
---
+++ private_src/timer/base_timer_handle.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "base/embedded/timer/base_timer_handle.h"
+
+class base::base_timer::base_timer_handle
+{
+public:
+};
