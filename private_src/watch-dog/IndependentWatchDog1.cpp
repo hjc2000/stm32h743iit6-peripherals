113246188a9bbbaccfbef760d6b154331e28859c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-22 22:35:45 +0800

    优化独立看门狗1超时计算逻辑，移除冗余变量定义

--- private_src/watch-dog/IndependentWatchDog1.cpp
+++ private_src/watch-dog/IndependentWatchDog1.cpp
@@ -11,10 +11,9 @@ void bsp::IndependentWatchDog1::Initialize(std::chrono::milliseconds const &time
 	_handle.Init.Window = 0x0fff;
 
 	base::unit::Second inner_clock_source_interval{_inner_clock_frequency};
-	base::unit::Second timeout_second{timeout};
 
 	// 所需的 (分频器计数值 + 计数器计数值)
-	int64_t total_count = static_cast<int64_t>(timeout_second / inner_clock_source_interval);
+	int64_t total_count = static_cast<int64_t>(base::unit::Second{timeout} / inner_clock_source_interval);
 
 	for (size_t i = 0; i < _prescaler_defines_count; i++)
 	{
