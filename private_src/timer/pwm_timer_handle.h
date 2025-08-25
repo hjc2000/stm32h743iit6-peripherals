6bb034b5d9f9a0f9ba3d50092f697a73713a00dc
hjc2000 <hjc.666@foxmail.com>
2025-08-25 15:37:08 +0800

    实现PWM定时器句柄核心功能，添加初始化模式、周期比较值及死区时间控制接口

--- private_src/timer/pwm_timer_handle.h
+++ private_src/timer/pwm_timer_handle.h
@@ -4,4 +4,27 @@
 class base::pwm_timer::pwm_timer_handle
 {
 public:
+	virtual void InitializeAsUpMode(base::pwm_timer::pwm_timer_handle &self,
+									base::unit::Hz const &frequency,
+									base::pwm_timer::Polarity effective_polarity) = 0;
+
+	virtual void InitializeAsDownMode(base::pwm_timer::pwm_timer_handle &self,
+									  base::unit::Hz const &frequency,
+									  base::pwm_timer::Polarity effective_polarity) = 0;
+
+	virtual void InitializeAsUpDownMode(base::pwm_timer::pwm_timer_handle &self,
+										base::unit::Hz const &frequency,
+										base::pwm_timer::Polarity effective_polarity) = 0;
+
+	virtual uint32_t Cycle(base::pwm_timer::pwm_timer_handle const &self) = 0;
+
+	virtual uint32_t CompareValue(base::pwm_timer::pwm_timer_handle const &self) = 0;
+
+	virtual void SetCompareValue(base::pwm_timer::pwm_timer_handle &self, uint32_t value) = 0;
+
+	virtual uint32_t DeadTime(base::pwm_timer::pwm_timer_handle const &self) = 0;
+
+	virtual void SetDeadTime(base::pwm_timer::pwm_timer_handle &self, uint32_t value) = 0;
+
+	virtual void Start(base::pwm_timer::pwm_timer_handle &self) = 0;
 };
