ba3e31d1aed790b2080079704d8f52cd51b7e1da
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-18 22:33:29 +0800

    refactor(fmc): 更新 clock_source.h 文件
    - 在 FMC 时钟源配置函数前添加注释，说明函数功能和参数
    - 注释中添加参考链接，便于开发人员进一步了解 FMC 时钟源配置

--- private_src/fmc/clock_source.h
+++ private_src/fmc/clock_source.h
@@ -3,6 +3,13 @@
 
 namespace bsp
 {
+	///
+	/// @brief 配置 FMC 时钟源。
+	///
+	/// @note https://blog.csdn.net/qq_36148047/article/details/151839506?sharetype=blogdetail&sharerId=151839506&sharerefer=PC&sharesource=qq_36148047&spm=1011.2480.3001.8118
+	///
+	/// @param clock_source_name
+	///
 	void config_fmc_clock_source(std::string const &clock_source_name);
 
 } // namespace bsp
