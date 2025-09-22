780b921b00c61c6e4823e93843c3927cf80b4106
hjc2000 <hjc.666@foxmail.com>
2025-09-22 16:12:09 +0800

    PllClockSource2

---
+++ private_src/clock/PllClockSource2.cpp
@@ -0,0 +1,189 @@
+#include "PllClockSource2.h" // IWYU pragma: keep
+#include "base/embedded/clock/ClockSource.h"
+#include "base/string/define.h"
+#include <cstdint>
+
+/* #region 静态的配置帮助函数 */
+
+uint32_t bsp::PllClockSource2::input_channel_name_to_define_value(std::string const &input_channel_name)
+{
+	if (input_channel_name == "hse")
+	{
+		return RCC_PLLSOURCE_HSE;
+	}
+	else if (input_channel_name == "hsi")
+	{
+		return RCC_PLLSOURCE_HSI;
+	}
+	else if (input_channel_name == "csi")
+	{
+		return RCC_PLLSOURCE_CSI;
+	}
+	else
+	{
+		throw std::invalid_argument{CODE_POS_STR + "非法输入通道名。"};
+	}
+}
+
+bsp::PllClockSource2::Factors bsp::PllClockSource2::get_factors(std::map<std::string, uint32_t> const &channel_factor_map)
+{
+	Factors ret{};
+
+	{
+		auto it = channel_factor_map.find("m");
+		if (it == channel_factor_map.end())
+		{
+			throw std::invalid_argument{CODE_POS_STR + "缺少 m 因子。"};
+		}
+
+		ret._m = it->second;
+	}
+
+	{
+		auto it = channel_factor_map.find("n");
+		if (it == channel_factor_map.end())
+		{
+			throw std::invalid_argument{CODE_POS_STR + "缺少 n 因子。"};
+		}
+
+		ret._n = it->second;
+	}
+
+	{
+		auto it = channel_factor_map.find("p");
+		if (it == channel_factor_map.end())
+		{
+			throw std::invalid_argument{CODE_POS_STR + "缺少 p 因子。"};
+		}
+
+		ret._p = it->second;
+	}
+
+	{
+		auto it = channel_factor_map.find("q");
+		if (it == channel_factor_map.end())
+		{
+			throw std::invalid_argument{CODE_POS_STR + "缺少 q 因子。"};
+		}
+
+		ret._q = it->second;
+	}
+
+	{
+		auto it = channel_factor_map.find("r");
+		if (it == channel_factor_map.end())
+		{
+			throw std::invalid_argument{CODE_POS_STR + "缺少 r 因子。"};
+		}
+
+		ret._r = it->second;
+	}
+
+	return ret;
+}
+
+base::unit::MHz bsp::PllClockSource2::get_input_frequency(std::string const &input_channel_name)
+{
+	if (input_channel_name == "hse")
+	{
+		base::clock::ClockSource clock_source{"hse"};
+		base::unit::MHz ret = clock_source.Frequency();
+		return ret;
+	}
+
+	if (input_channel_name == "hsi")
+	{
+		base::clock::ClockSource clock_source{"hsi"};
+		base::unit::MHz ret = clock_source.Frequency();
+		return ret;
+	}
+
+	if (input_channel_name == "csi")
+	{
+		base::clock::ClockSource clock_source{"csi"};
+		base::unit::MHz ret = clock_source.Frequency();
+		return ret;
+	}
+
+	throw std::invalid_argument{CODE_POS_STR + "非法输入通道名。"};
+}
+
+uint32_t bsp::PllClockSource2::calculate_pll_range(base::unit::MHz const &m_channel_output_frequency)
+{
+	if (m_channel_output_frequency < base::unit::MHz{2})
+	{
+		return RCC_PLL1VCIRANGE_0;
+	}
+
+	if (m_channel_output_frequency >= base::unit::MHz{2} && m_channel_output_frequency < base::unit::MHz{4})
+	{
+		return RCC_PLL1VCIRANGE_1;
+	}
+
+	if (m_channel_output_frequency >= base::unit::MHz{4} && m_channel_output_frequency < base::unit::MHz{8})
+	{
+		return RCC_PLL1VCIRANGE_2;
+	}
+
+	return RCC_PLL1VCIRANGE_3;
+}
+
+/* #endregion */
+
+base::unit::MHz bsp::PllClockSource2::Frequency(std::string const &output_channel_name) const
+{
+	if (!_configured)
+	{
+		throw std::runtime_error{CODE_POS_STR + "必须先通过本类配置后才能查看频率。"};
+	}
+
+	if (output_channel_name == "p")
+	{
+		return _p_freq;
+	}
+
+	if (output_channel_name == "q")
+	{
+		return _q_freq;
+	}
+
+	if (output_channel_name == "r")
+	{
+		return _r_freq;
+	}
+
+	throw std::invalid_argument{CODE_POS_STR + "非法输出通道。"};
+}
+
+void bsp::PllClockSource2::Configure(std::string const &input_channel_name,
+									 std::map<std::string, uint32_t> const &channel_factor_map)
+{
+	Factors factors = get_factors(channel_factor_map);
+	base::unit::MHz input_frequency = get_input_frequency(input_channel_name);
+	uint32_t pll_range = calculate_pll_range(input_frequency / factors._m);
+
+	RCC_OscInitTypeDef def{};
+	def.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+	def.PLL.PLLState = RCC_PLL_ON;
+	def.PLL.PLLSource = input_channel_name_to_define_value(input_channel_name);
+	def.PLL.PLLRGE = pll_range;
+	def.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+	def.PLL.PLLM = factors._m;
+	def.PLL.PLLN = factors._n;
+	def.PLL.PLLP = factors._p;
+	def.PLL.PLLQ = factors._q;
+	def.PLL.PLLR = factors._r;
+
+	HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
+	if (result != HAL_StatusTypeDef::HAL_OK)
+	{
+		throw std::runtime_error{CODE_POS_STR + "配置 PLL 失败。"};
+	}
+
+	// 打开后，记录各个输出通道的频率
+	_p_freq = input_frequency / factors._m * factors._n / factors._p;
+	_q_freq = input_frequency / factors._m * factors._n / factors._q;
+	_r_freq = input_frequency / factors._m * factors._n / factors._r;
+
+	_configured = true;
+}
---
+++ private_src/clock/PllClockSource2.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "base/unit/MHz.h"
+#include "clock_source_handle.h"
+#include "hal.h"
+#include <cstdint>
+#include <string>
+
+namespace bsp
+{
+	class PllClockSource2 final :
+		public base::clock::clock_source_handle
+	{
+	private:
+		struct Factors
+		{
+			uint32_t _m{};
+			uint32_t _n{};
+			uint32_t _p{};
+			uint32_t _q{};
+			uint32_t _r{};
+		};
+
+		inline static bool _configured = false;
+		inline static base::unit::MHz _p_freq;
+		inline static base::unit::MHz _q_freq;
+		inline static base::unit::MHz _r_freq;
+
+		static uint32_t input_channel_name_to_define_value(std::string const &input_channel_name);
+
+		static Factors get_factors(std::map<std::string, uint32_t> const &channel_factor_map);
+
+		static base::unit::MHz get_input_frequency(std::string const &input_channel_name);
+
+		///
+		/// @brief
+		///
+		/// @param m_channel_output_frequency m 通道的输出频率。
+		/// @return
+		///
+		static uint32_t calculate_pll_range(base::unit::MHz const &m_channel_output_frequency);
+
+	public:
+		virtual base::unit::MHz Frequency(std::string const &output_channel_name) const override;
+
+		virtual void Configure(std::string const &input_channel_name,
+							   std::map<std::string, uint32_t> const &channel_factor_map) override;
+
+		virtual void TurnOff() override
+		{
+			RCC_OscInitTypeDef def{};
+			def.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+			def.PLL.PLLState = RCC_PLL_OFF;
+			HAL_StatusTypeDef result = HAL_RCC_OscConfig(&def);
+			if (result != HAL_StatusTypeDef::HAL_OK)
+			{
+				throw std::runtime_error{"关闭 PLL 失败。"};
+			}
+
+			_configured = false;
+		}
+	};
+
+} // namespace bsp
