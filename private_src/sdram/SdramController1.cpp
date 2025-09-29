844ee81a658215a61d0b4437f96d08cd7ac628b9
hjc2000 <hjc.666@foxmail.com>
2025-09-29 16:40:35 +0800

    fix(sdram): 调整MPU保护区域参数格式
    调整SdramController1中mpu_set_protection函数调用的参数格式，将MPU区域号和访问权限参数分行显示，提高代码可读性。

--- private_src/sdram/SdramController1.cpp
+++ private_src/sdram/SdramController1.cpp
@@ -13,9 +13,10 @@ void bsp::SdramController1::InitializeAsReadBurstMode(std::string const &clock_s
 													  base::sdram::ReadBurstLength const &read_burst_length)
 {
 	/* 保护SDRAM区域,共32M字节 */
-	mpu_set_protection(0xC0000000,               /* 基地址 */
-					   MPU_REGION_SIZE_32MB,     /* 长度 */
-					   MPU_REGION_NUMBER6, 0,    /* NUMER6,允许指令访问 */
+	mpu_set_protection(0xC0000000,           /* 基地址 */
+					   MPU_REGION_SIZE_32MB, /* 长度 */
+					   MPU_REGION_NUMBER6,
+					   0,                        /* NUMER6,允许指令访问 */
 					   MPU_REGION_FULL_ACCESS,   /* 全访问 */
 					   MPU_ACCESS_NOT_SHAREABLE, /* 禁止共用 */
 					   MPU_ACCESS_CACHEABLE,     /* 允许cache */
