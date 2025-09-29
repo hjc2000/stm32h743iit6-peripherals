d7330ecdeff43352ad29a240e1b4b2c1110bf6d1
hjc2000 <hjc.666@foxmail.com>
2025-09-29 16:41:28 +0800

    fix(sdram): 更新MPU配置参数命名
    将MPU配置中的数字常量0替换为具名常量MPU_INSTRUCTION_ACCESS_ENABLE，
    提高代码可读性和维护性。此修改不影响实际功能逻辑。

--- private_src/sdram/SdramController1.cpp
+++ private_src/sdram/SdramController1.cpp
@@ -16,11 +16,11 @@ void bsp::SdramController1::InitializeAsReadBurstMode(std::string const &clock_s
 	mpu_set_protection(0xC0000000,           /* 基地址 */
 					   MPU_REGION_SIZE_32MB, /* 长度 */
 					   MPU_REGION_NUMBER6,
-					   0,                        /* NUMER6,允许指令访问 */
-					   MPU_REGION_FULL_ACCESS,   /* 全访问 */
-					   MPU_ACCESS_NOT_SHAREABLE, /* 禁止共用 */
-					   MPU_ACCESS_CACHEABLE,     /* 允许cache */
-					   MPU_ACCESS_BUFFERABLE);   /* 允许缓冲 */
+					   MPU_INSTRUCTION_ACCESS_ENABLE, /* NUMER6,允许指令访问 */
+					   MPU_REGION_FULL_ACCESS,        /* 全访问 */
+					   MPU_ACCESS_NOT_SHAREABLE,      /* 禁止共用 */
+					   MPU_ACCESS_CACHEABLE,          /* 允许cache */
+					   MPU_ACCESS_BUFFERABLE);        /* 允许缓冲 */
 
 	__HAL_RCC_FMC_CLK_ENABLE();
 
