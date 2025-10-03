9c6dbd9cc89f8e699520e1bb1a7272924cb70e0a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-10-03 18:36:21 +0800

    refactor(usb): 重构 USB CDC 串口初始化逻辑
    将 `UsbCdcSerialPort` 的构造函数实现从 `.cpp` 文件移至 `.h` 文件内联定义，
    同时清理了 `usb_device.cpp` 和 `usb_device.h` 中冗余的包含和注释代码，
    使 USB 设备初始化流程更加紧凑和清晰。

--- private_src/usb/USB_DEVICE/App/usb_device.cpp
+++ private_src/usb/USB_DEVICE/App/usb_device.cpp
@@ -1,35 +1,7 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : usb_device.c
- * @version        : v1.0_Cube
- * @brief          : This file implements the USB Device
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2025 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
-
 #include "usb_device.h"
 #include "base/embedded/usb/fs-device-pcd/UsbFsDevicePcd.h"
-#include "base/string/define.h"
 #include "UsbCdcSerialPort.h"
-#include "usbd_cdc.h"
-#include "usbd_cdc_if.h"
-#include "usbd_core.h"
-#include "usbd_desc.h"
 #include <memory>
-#include <stdexcept>
 
 /**
  * Init USB device Library, add supported class and start the library
--- private_src/usb/UsbCdcSerialPort.h
+++ private_src/usb/UsbCdcSerialPort.h
@@ -37,7 +37,33 @@ namespace bsp
 		std::shared_ptr<base::usb::fs_device_pcd::UsbFsDevicePcd> _pcd;
 
 	public:
-		UsbCdcSerialPort(std::shared_ptr<base::usb::fs_device_pcd::UsbFsDevicePcd> const &pcd);
+		UsbCdcSerialPort(std::shared_ptr<base::usb::fs_device_pcd::UsbFsDevicePcd> const &pcd)
+		{
+			_pcd = pcd;
+
+			/* Init Device Library, add supported class and start the library. */
+			if (USBD_Init(&_handle_context._handle, &FS_Desc, 0) != USBD_OK)
+			{
+				throw std::runtime_error{CODE_POS_STR + "初始化失败。"};
+			}
+
+			if (USBD_RegisterClass(&_handle_context._handle, &USBD_CDC) != USBD_OK)
+			{
+				throw std::runtime_error{CODE_POS_STR + "初始化失败。"};
+			}
+
+			if (USBD_CDC_RegisterInterface(&_handle_context._handle, &USBD_Interface_fops_FS) != USBD_OK)
+			{
+				throw std::runtime_error{CODE_POS_STR + "初始化失败。"};
+			}
+
+			if (USBD_Start(&_handle_context._handle) != USBD_OK)
+			{
+				throw std::runtime_error{CODE_POS_STR + "初始化失败。"};
+			}
+
+			HAL_PWREx_EnableUSBVoltageDetector();
+		}
 
 		USBD_HandleTypeDef &UsbdHandle()
 		{
