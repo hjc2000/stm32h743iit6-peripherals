e7d4ae2d5a0700bb6bb6e19b06a616f489f02d85
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 03:07:14 +0800

    DisableGlobalInterruptionImplementation

--- private_src/interrupt/interrupt.cpp
+++ private_src/interrupt/interrupt.cpp
@@ -2,12 +2,12 @@
 #include "base/embedded/interrupt/interrupt.h"
 #include "hal.h"
 
-void base::interrupt::DisableGlobalInterruptionImpelement::disable_global_interrupt() noexcept
+void base::interrupt::DisableGlobalInterruptionImplementation::disable_global_interrupt() noexcept
 {
 	__disable_irq();
 }
 
-void base::interrupt::DisableGlobalInterruptionImpelement::enable_global_interrupt() noexcept
+void base::interrupt::DisableGlobalInterruptionImplementation::enable_global_interrupt() noexcept
 {
 	__enable_irq();
 }
